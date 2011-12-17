8b7ce28840552befeb557d173ae4e827054ed4e5
Peter Alfonso <peteralfonsojr@gmail.com>
2011-12-17 14:46:43 -0500

    Update build number and some applications
    Maps V. 6.0.3
    Market V. 3.4.4
    Music V. 4.1.511
    Videos V. 1.4.2
    PlusOne V. 2.2.0.233662

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,3 +1,3 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=ICL53F BUILD_VERSION_TAGS=release-keys USER=android-build
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build
 
