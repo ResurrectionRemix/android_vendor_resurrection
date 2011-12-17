d07e628d7d19581370cc0b71832302ff59ca76b2
Peter Alfonso <peteralfonsojr@gmail.com>
2011-12-17 15:00:25 -0500

    Add missing common properties

--- products/common.mk
+++ products/common.mk
@@ -14,7 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.feedback \
-    ro.setupwizard.enable_bypass=1 \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.mode=OPTIONAL \
     ro.setupwizard.enterprise_mode=1
 
 # Blobs common to all devices
--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,3 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build
 
+# Rom Manager properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rommanager.developerid=pete
+
