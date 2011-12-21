a76c614f96e33e4a73ce10da33538adbb0e8d4f4
Peter Alfonso <peteralfonsojr@gmail.com>
2011-12-20 19:44:00 -0500

    Fix whitespacing

--- products/crespo4g.mk
+++ products/crespo4g.mk
@@ -12,6 +12,7 @@ PRODUCT_MODEL := Nexus S 4G
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=sojus BUILD_ID=GWK74 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
+
 # Inherit common build.prop overrides
 -include vendor/pete/products/common_versions.mk
 
