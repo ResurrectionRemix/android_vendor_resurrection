a29c3f690b923071385df7c266b2a5b97ec4af4b
Jacob Bell <jacob@fzservers.com>
2012-01-31 21:36:58 -0500

    Add tenderloin

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_maguro-userdebug
+add_lunch_combo aokp_tenderloin-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_p4wifi-userdebug
