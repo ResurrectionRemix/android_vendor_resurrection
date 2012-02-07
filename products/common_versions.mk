04a7bd41ef7151f77c36a531a8b1e4a652088040
Roman Birg <romanbirg@gmail.com>
2012-02-07 10:12:10 -0600

    nothing to see here

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -3,6 +3,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=relea
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(TARGET_PRODUCT)_milestone-3 \
+    ro.modversion=$(TARGET_PRODUCT)_build-22 \
     ro.rommanager.developerid=teamkang
 
