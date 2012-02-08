63701d6abe1742f456de610ec63ffec7487d7171
Roman Birg <romanbirg@gmail.com>
2012-02-08 01:28:15 -0600

    fix lunch combos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,11 @@
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_maguro-userdebug
-add_lunch_combo aokp_tenderloin-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_p4wifi-userdebug
+add_lunch_combo aokp_tenderloin-userdebug
 add_lunch_combo aokp_vivow-userdebug
+add_lunch_combo aokp_p4-userdebug
+add_lunch_combo aokp_p4vzw-userdebug
+add_lunch_combo aokp_stingray-userdebug
+add_lunch_combo aokp_wingray-userdebug
