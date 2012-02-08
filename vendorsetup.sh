05e3340da8c75eac65f6cf451dda6130742544ee
Jacob Bell <jacob@fzservers.com>
2012-02-08 17:31:15 -0500

    Add OG Inc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
+add_lunch_combo aokp_inc-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_p4-userdebug
 add_lunch_combo aokp_p4wifi-userdebug
