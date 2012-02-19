116cae57c3a6f3141d7364346496a56ffb041eaa
Roman Birg <romanbirg@gmail.com>
2012-02-19 17:20:15 -0600

    build maguro/toro first

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,8 @@
+add_lunch_combo aokp_maguro-userdebug
+add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_inc-userdebug
-add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_otter-userdebug
 add_lunch_combo aokp_p4-userdebug
 add_lunch_combo aokp_p4vzw-userdebug
@@ -9,7 +10,6 @@ add_lunch_combo aokp_p4wifi-userdebug
 add_lunch_combo aokp_stingray-userdebug
 add_lunch_combo aokp_supersonic-userdebug
 add_lunch_combo aokp_tenderloin-userdebug
-add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_vzwtab-userdebug
 add_lunch_combo aokp_vivow-userdebug
 add_lunch_combo aokp_wingray-userdebug
