93299fbd194c461a072fd0f5e2f6b20cf1e3e91d
Roman Birg <romanbirg@gmail.com>
2012-02-29 07:59:35 -0600

    just kidding; 27

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,13 +4,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.modversion=$(TARGET_PRODUCT)_milestone-4
+    ro.modversion=$(TARGET_PRODUCT)_build-27
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.board=$TARGET_BUILD_DEVICE \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=3
+    ro.goo.version=4
 
 
