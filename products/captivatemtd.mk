a325485846dd75afacce46d21b4c5845e66f6df1
Roman Birg <romanbirg@gmail.com>
2012-02-29 02:12:45 -0600

    add bootanimations to sgs and captivate

--- products/captivatemtd.mk
+++ products/captivatemtd.mk
@@ -17,3 +17,7 @@ PRODUCT_MODEL := SGH-I897
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I897 TARGET_DEVICE=SGH-I897 BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
