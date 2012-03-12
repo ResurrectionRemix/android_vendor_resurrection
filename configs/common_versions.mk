4d8320ac05a701ed81d1e8c3639ef039aea846a9
Roman Birg <romanbirg@gmail.com>
2012-03-11 23:41:48 -0500

    p99x workaround for data

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,10 +1,21 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-# Rom Manager properties
+CURRENT_VERSION=$(TARGET_PRODUCT)_milestone-4
+
+ifeq ($(TARGET_DEVICE),p999)
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=$(CURRENT_VERSION)
+else ifeq ($(TARGET_DEVICE),p990)
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=$(CURRENT_VERSION)
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.cm.version=$(CURRENT_VERSION)
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=teamkang \
-    ro.modversion=$(TARGET_PRODUCT)_milestone-4
+    ro.rommanager.developerid=teamkang
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
