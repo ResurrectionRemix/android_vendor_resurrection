a27e8a34e234512d8288035508c72e064c7213a2
Roman Birg <romanbirg@gmail.com>
2012-03-18 12:17:01 -0500

    include AppWidgetPicker with every device

--- configs/common.mk
+++ configs/common.mk
@@ -7,7 +7,8 @@ PRODUCT_PACKAGES += \
     NoiseField \
     ROMControl \
     PhaseBeam \
-    su
+    su \
+    AppWidgetPicker
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
