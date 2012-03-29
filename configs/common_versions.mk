a5f66b39e1126f6192e14466e5be38568c2288b3
Roman Birg <romanbirg@gmail.com>
2012-03-29 06:25:31 -0500

    bump version

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)_build-28
+CURRENT_VERSION=$(TARGET_PRODUCT)_build-29
 
 ifneq ($(TARGET_PRODUCT),aokp_p999)
     ifeq ($(TARGET_PRODUCT),aokp_p990)
@@ -23,6 +23,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=5
+    ro.goo.version=6
 
 
