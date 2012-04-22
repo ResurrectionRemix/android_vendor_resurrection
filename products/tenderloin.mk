020c1bed7ed59a44b4d986196f3947e51dcadf68
Will DeBerry <hillbillyhacker86@gmail.com>
2012-04-22 02:43:50 -0400

    Add ROMControl overlay for tenderloin
    Does not have torch/hardware buttons/fastcharge/color tuning
    
    Change-Id: I0e0854d83302672ac27c3a511444928d67627ac9

--- products/tenderloin.mk
+++ products/tenderloin.mk
@@ -4,6 +4,8 @@ PRODUCT_RELEASE_NAME := Touchpad
 
 $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/tenderloin
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=IML74K BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys" BUILD_NUMBER=189904
 
 PRODUCT_NAME := aokp_tenderloin
