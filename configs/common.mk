e7888efd898aedd537af241cc81df555d8568e13
K900 <k0009000@gmail.com>
2012-05-04 12:47:42 +0400

    Enable LWPs globally for all devices
    Change-Id: I6ab50e931cc3dc161888eb5d41f382a182a10c09

--- configs/common.mk
+++ configs/common.mk
@@ -75,5 +75,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Live wallpapers for all
+PRODUCT_PACKAGES += \
+		LiveWallpapers \
+		LiveWallpapersPicker \
+		MagicSmokeWallpapers \
+		VisualizationWallpapers \
+		librs_jni
+
+PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
 # Inherit common build.prop overrides
 -include vendor/aokp/configs/common_versions.mk
