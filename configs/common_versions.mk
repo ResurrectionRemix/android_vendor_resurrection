0302f90d631c28fa64e07cfc01773a5f4551e7e1
Roman Birg <romanbirg@gmail.com>
2012-05-14 14:26:22 -0500

    bump to build 36
    Change-Id: I0d233ecc916c0bada9eec0a41ae8b78780242c28

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,13 +4,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-35
+    ro.aokp.version=$(TARGET_PRODUCT)_build-36
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=aokp \
-    ro.goo.version=13
+    ro.goo.version=14
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
