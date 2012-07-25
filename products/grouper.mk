3856b51366b7965096bc65aae68d55ee68d14730
Mike Wilson <mwilson@acesappraisal.com>
2012-07-25 11:21:55 -0700

    Fixes for Grouper
    Change-Id: I24984ef33843852c64fbafa6994c8983bf6ebb3b
    Signed-off-by: Mike Wilson <mwilson@acesappraisal.com>

--- products/grouper.mk
+++ products/grouper.mk
@@ -13,3 +13,7 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JRO03C/398337:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03C 398337 release-keys" BUILD_NUMBER=398337
 
+# Grouper specific packages
+	PRODUCT_PACKAGES += \
+	bootanimation_720_1280 \
+	NovaLauncher
