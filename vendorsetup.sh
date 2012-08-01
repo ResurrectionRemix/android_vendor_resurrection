e3c45e12a1ead82bac19f6ee2155a3cd178443eb
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-07-31 17:57:42 -0700

    vendorsetup: let's keep everything alphabetical please
    Change-Id: I5bf498d06187f52e7a8cdfad1789e06ccbcca872
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
-add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_stingray-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
