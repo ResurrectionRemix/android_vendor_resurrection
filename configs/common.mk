AOKP/vendor_aokp

Update apns-conf and use it
Change-Id: I6e6b978e4603afa5a464e42fe7601946b3ca371c
Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

configs/common.mk
@@ -38,6 +38,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
+    vendor/aokp/prebuilt/common/etc/apns-conf.conf:system/etc/apns-conf.xml \
     vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
 
 # init.d

ResurrectionRemix/android_vendor_resurrection

  Patch Set 2: Remove unnecessary commit dependency.
  Patch Set 3: Le Derp

configs/common.mk
@@ -38,6 +38,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
+    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
 
 # init.d