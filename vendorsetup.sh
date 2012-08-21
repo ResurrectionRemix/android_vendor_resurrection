c45528c249d7ef5902e369da3843cf4ca3dc7a98
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-08-20 21:17:56 -0700

    vendor: instate anti-Nexus racism
    p2: moved grouper
    
    Change-Id: I44ed08a9ec54191dd04cc0ee34875ad8205dd869
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,12 @@
+add_lunch_combo aokp_maguro-userdebug
+add_lunch_combo aokp_toro-userdebug
+add_lunch_combo aokp_toroplus-userdebug
+add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
-add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_hercules-userdebug
-add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_p3113-userdebug
 add_lunch_combo aokp_p5113-userdebug
 add_lunch_combo aokp_stingray-userdebug
-add_lunch_combo aokp_toro-userdebug
-add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_wingray-userdebug
