f12e13953a6dd571a59ae238e1ad2b829e517bad
Roman Birg <romanbirg@gmail.com>
2012-09-11 10:35:31 -0500

    update versioning to reflect official/unofficial builds
    uses $AOKP_BUILD whether to determine the versioning info.
    $AOKP_BUILD=<goo version int>/<build string>
    
    Change-Id: Ia55a99135dd0e288491884d055064d68f5242182
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -3,14 +3,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=relea
 
 DATE = $(shell date +%h-%d-%y)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1
-
-# Goo updater app
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aokp \
-    ro.goo.rom=aokp \
-    ro.goo.version=21
+ifneq ($(AOKP_BUILD),)
+	# AOKP_BUILD=<goo version int>/<build string>
+	PRODUCT_PROPERTY_OVERRIDES += \
+	    ro.goo.developerid=aokp \
+	    ro.goo.rom=aokp \
+	    ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
+		ro.aokp.version=$(TARGET_PRODUCT)_jb_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
+else
+	PRODUCT_PROPERTY_OVERRIDES += \
+		ro.aokp.version=$(TARGET_PRODUCT)_unofficial_$(DATE)
+endif
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
