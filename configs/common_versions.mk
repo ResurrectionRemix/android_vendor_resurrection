23f49273e02a065485520d28790bdde61d1d10a4
K900 <k0009000@gmail.com>
2012-09-18 19:47:33 +0400

    Fix date locale in AOKP version
    Change-Id: I81fb5bbed7a0ecc51657a641732d6d9b0cdea4ee

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell date +%h-%d-%y)
+DATE = $(shell vendor/aokp/tools/getdate)
 
 ifneq ($(AOKP_BUILD),)
 	# AOKP_BUILD=<goo version int>/<build string>
