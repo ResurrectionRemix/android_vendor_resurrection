38010fa9d28098173f4450c5c88c1053df93de5b
Gergely Szell <sethyx@gmail.com>
2012-09-29 09:34:05 +0200

    how did you even disappear, captivate?
    Change-Id: I489586288a40c9a97c13675b648e91384c99e9aa
    Signed-off-by: Gergely Szell <sethyx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
+add_lunch_combo aokp_captivatemtd-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
