ec56fa57acfeb68a495ce199b10396243e8c3cd4
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-10-14 12:51:58 -0400

    VZW: Add VerizonSSO.apk, add MOAR devices
    This is final piece of whats needed for everything to work. This is also found on the market, but it has to be a system/app/ for everything to work as it should.
    
    PS2: Add toro, fascinatemtd(still requires files in vendor_samsung)
    PS3: Remove fascinatemtd for now. Toro and d2's are good to merge.
    
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>
    Change-Id: I1497ac3f942d9f67ed246e7e6c4c7d059bb4b565

--- products/toro.mk
+++ products/toro.mk
@@ -4,6 +4,9 @@ $(call inherit-product, device/samsung/toro/full_toro.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
+# Inherit common Verizon Wireless Files
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/tuna
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/toro
 
