231d3d759b33a0d5078ff5542064bb3b987e42b7
Bajee11 <bajee11@gmail.com>
2012-10-19 17:31:08 +0200

    d2att: update build fingerprint
    Signed-off-by: Bajee11 <bajee11@gmail.com>
    Change-Id: Ibdab32d22078152f5e98f8eab1a4b9ea3ec4227a

--- products/d2att.mk
+++ products/d2att.mk
@@ -16,7 +16,7 @@ PRODUCT_DEVICE := d2att
 PRODUCT_MODEL := SGH-I747
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2att-user 4.0.4 UCALH9 I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2uc/d2att:4.0.4/UCALH9/I747UCALEM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT=samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALH9:user/release-keys PRIVATE_BUILD_DESC="d2att-user 4.0.4 IMM76D I747UCALH9 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
