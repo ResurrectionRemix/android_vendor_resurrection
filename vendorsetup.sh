d7c128a40f988c9153c2d2d894923d76453190f9
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-02 22:29:46 +0100

    No love for manta & toroplus YET
    Change-Id: I216217f1c891a8d299d974dcca7d2eb5e6cb1cbb
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_mako-userdebug
-add_lunch_combo aokp_manta-userdebug
+# add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_toro-userdebug
-add_lunch_combo aokp_toroplus-userdebug
+# add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
