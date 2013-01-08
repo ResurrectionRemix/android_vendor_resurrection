2fc70288ef9c1f422cb25115530403d7f2217f8e
Stevespear426 <stephen.k.spear@gmail.com>
2013-01-08 23:56:38 +0100

    Add Tilapia Products
    Change-Id: Ic4d57296df2c05cd0c855faf43cc3bff2060dff6
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

---
+++ products/tilapia.mk
@@ -0,0 +1,23 @@
+# Inherit AOSP device configuration for grouper
+$(call inherit-product, device/asus/tilapia/full_tilapia.mk)
+
+# Inherit AOKP common_tablet bits
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Grouper Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/grouper
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_tilapia
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := tilapia
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.2.1 JOP40D 533553 release-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,4 @@ add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_toro-userdebug
 # add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_tilapia-userdebug
