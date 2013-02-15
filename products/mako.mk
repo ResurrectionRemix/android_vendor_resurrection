fd888a5630003829f9527ea68f77a629c29ed31f
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-02-15 19:39:18 +0100

    Update mako fingerprint
    Change-Id: I995b631723aea28246c3c0b53334eb5fa0451fae
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- products/mako.mk
+++ products/mako.mk
@@ -17,7 +17,7 @@ PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2 JOP40C 527662 release-keys" BUILD_NUMBER=527662
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
