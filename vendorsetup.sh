9b84a3dc4d00d2de74aec2ab60788e47b5b03a82
Gergely Szell <sethyx@gmail.com>
2013-02-18 10:12:25 +0100

    Enable toroplus
    Change-Id: Idf568af7ac8146d5a1c14423fad46b9cff89c03f
    Signed-off-by: Gergely Szell <sethyx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
+add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
 add_lunch_combo aokp_manta-userdebug
-# add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_a510-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
