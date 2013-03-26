357b694123ce332ff3343991f60affe288888888
sethyx <sethyx@gmail.com>
2013-03-26 10:20:51 +0100

    Remove a510 for now
    Change-Id: I8c9e6db59889fd6e1ffac5838faa379d40ee179c
    Signed-off-by: sethyx <sethyx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
 add_lunch_combo aokp_manta-userdebug
-add_lunch_combo aokp_a510-userdebug
+# add_lunch_combo aokp_a510-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
