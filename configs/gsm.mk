3f178a443bd537c40368975b2c1840945893a5a6
Resurrection Remix <altan_koruk@hotmail.com>
2013-03-29 22:48:12 +0200

    Update gsm.mk

--- configs/gsm.mk
+++ configs/gsm.mk
@@ -1,6 +1,6 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/rr/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
