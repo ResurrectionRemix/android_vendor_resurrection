9c9fa63859377a7d7ded135e80bfc34d4ea17c75
altan <korukaltan@gmail.com>
2013-04-21 09:15:14 +0300

    added n7100 support 2
    Change-Id: I6ecae92a935ffad744915c9cb79ea29854f3afa7

---
+++ products/n7100.mk
@@ -0,0 +1,25 @@
+# Inherit AOSP device configuration for hercules.
+$(call inherit-product, device/samsung/n7100/full_n7100.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/rr/configs/common.mk)
+# Inherit GSM common stuff
+$(call inherit-product, vendor/rr/configs/gsm.mk)
+
+# skyrocket overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/rr/overlay/common
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rr_n7100
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := n7100
+PRODUCT_MODEL := GT-N7100
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.1.1/JRO03C/N7100XXALJ3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.1.1 JRO03C N7100XXALJ3 release-keys"
+PRODUCT_RELEASE_NAME := n7100
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/rr/prebuilt/bootanimation/xhdpi/bootanimation.zip:system/media/bootanimation.zip
