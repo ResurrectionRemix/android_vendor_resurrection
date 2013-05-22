AOKP/vendor_aokp

aokp: add Xperia Z, ZL
Change-Id: If5539a97c3aa327c90ecd253968dac8a927c32b5
Signed-off-by: ChampionSwimmer <android@championswimmer.tk>

products/odin.mk
@@ -1,0 +1,26 @@
+# Inherit AOSP device configuration for odin
+$(call inherit-product-if-exists, device/sony/odin/full_odin.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# hayabusa overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/odin
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_odin
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := odin
+PRODUCT_MODEL := C6503
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503_1271-0336 BUILD_FINGERPRINT=Sony/C6503_1271-0336/C6503:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1980.zip:system/media/bootanimation-alt.zip

products/yuga.mk
@@ -1,0 +1,26 @@
+# Inherit AOSP device configuration for yuga
+$(call inherit-product-if-exists, device/sony/yuga/full_yuga.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# hayabusa overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/yuga
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_yuga
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := yuga
+PRODUCT_MODEL := C6603
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603_1270-4975 BUILD_FINGERPRINT=Sony/C6603_1270-4975/C6603:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1980.zip:system/media/bootanimation-alt.zip

ResurrectionRemix/android_vendor_resurrection

Conflicts:
	products/AndroidProducts.mk
	vendorsetup.sh

products/odin.mk
@@ -1,0 +1,26 @@
+# Inherit AOSP device configuration for odin
+$(call inherit-product-if-exists, device/sony/odin/full_odin.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# hayabusa overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/odin
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_odin
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := odin
+PRODUCT_MODEL := C6503
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503_1271-0336 BUILD_FINGERPRINT=Sony/C6503_1271-0336/C6503:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip

products/yuga.mk
@@ -1,0 +1,26 @@
+# Inherit AOSP device configuration for yuga
+$(call inherit-product-if-exists, device/sony/yuga/full_yuga.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# hayabusa overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/yuga
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_yuga
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := yuga
+PRODUCT_MODEL := C6603
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603_1270-4975 BUILD_FINGERPRINT=Sony/C6603_1270-4975/C6603:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip