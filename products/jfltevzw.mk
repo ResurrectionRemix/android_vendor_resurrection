161021d3418d30d475d6d6b36bba1ed11082964c
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-05-26 17:51:25 +0200

    jfltevzw: Fixup formatting and prop overrides
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    Change-Id: I74e45c46dd6306e91eacf05f474b99666e0a2926

--- products/jfltevzw.mk
+++ products/jfltevzw.mk
@@ -1,8 +1,11 @@
-# Inherit AOSP device configuration for jflteusc
+# Inherit AOSP device configuration for jfltevzw
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
 # Inherit common cdma apns
 $(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common.mk)
@@ -14,8 +17,7 @@ PRODUCT_DEVICE := jfltevzw
 PRODUCT_MODEL := SCH-I545
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="samsung/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" 
-PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
 
 # boot animation
 PRODUCT_COPY_FILES += \
