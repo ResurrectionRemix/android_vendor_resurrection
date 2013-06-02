9d4d1b2e97960df7154e1f63464812b39c721c6c
altan <korukaltan@gmail.com>
2013-06-02 22:52:54 +0300

    fix att m7 fingerprint
    Change-Id: I9f23bf4a3bc8a38e4bebea4fddcbcaddefc9224c

--- products/m7att.mk
+++ products/m7att.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 $(call inherit-product, device/htc/m7att/full_m7att.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="cingular_us/m7/m7:4.1.2/JZO54K/166754.12:user/release-keys" PRIVATE_BUILD_DESC="1.26.502.12 CL166754 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="cingular_us/m7/m7:4.1.2/JZO54K/166754.12:user/release-keys" PRIVATE_BUILD_DESC="1.26.502.12 CL166754 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
