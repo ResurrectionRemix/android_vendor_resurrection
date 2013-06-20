AOKP/vendor_aokp

jflteusc: Add support for USCC GS4 variant
Change-Id: I2fec5e527b8799afc810dd8479115c43fa09071b
Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

products/jflteusc.mk
@@ -1,0 +1,21 @@
+# Inherit AOSP device configuration for jflteusc
+$(call inherit-product, device/samsung/jflteusc/full_jflteusc.mk)
+
+# Inherit common cdma apns
+$(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_jflteusc
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := jflteusc
+PRODUCT_MODEL := SCH-R970
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMDB release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+vendor/aokp/prebuilt/bootanimation/bootanimation_1920_1280.zip:system/media/bootanimation-alt.zip

ResurrectionRemix/android_vendor_resurrection

Conflicts:
	vendorsetup.sh

products/jflteusc.mk
@@ -1,0 +1,21 @@
+# Inherit AOSP device configuration for jflteusc
+$(call inherit-product, device/samsung/jflteusc/full_jflteusc.mk)
+
+# Inherit common cdma apns
+$(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_jflteusc
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := jflteusc
+PRODUCT_MODEL := SCH-R970
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMDB release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+       vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip