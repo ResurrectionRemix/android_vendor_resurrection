b8c1736a84f80768f44c08059f77669fdad80f44
Stevespear426 <stephen.k.spear@gmail.com>
2013-06-26 02:34:30 +0200

    Update Manta fingerprint.
    We update the blobs months ago...
    
    Change-Id: Id9baa0ed5f4141b4c106fbe7f26f69391b0bdfdb
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- products/manta.mk
+++ products/manta.mk
@@ -14,7 +14,7 @@ PRODUCT_DEVICE := manta
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.1/JOP40D/533553:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.1 JOP40D 533553 release-keys" BUILD_NUMBER=533553
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
 
 PRODUCT_COPY_FILES +=  \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation-alt.zip
