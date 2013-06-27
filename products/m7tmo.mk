dbf2f63046e872ceaf266f7cca6546199f0f6030
Roman Birg <romanbirg@gmail.com>
2013-06-27 09:55:15 -0500

    m7tmo: update build fingerprint
    Change-Id: Ibb96a7518178157599c917a158ed37931cf1ee7a

--- products/m7tmo.mk
+++ products/m7tmo.mk
@@ -8,7 +8,7 @@ $(call inherit-product, device/htc/m7tmo/full_m7tmo.mk)
 
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PPRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="tmous/m7/m7:4.1.2/JZO54K/170484.7:user/release-keys" PRIVATE_BUILD_DESC="1.27.531.7 CL170484 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PPRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="tmous/m7/m7:4.1.2/JZO54K/177101.16:user/release-keys" PRIVATE_BUILD_DESC="1.29.531.16 CL177101 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
