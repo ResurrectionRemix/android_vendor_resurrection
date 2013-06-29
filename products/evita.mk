ad73dcfbab998c830f9d0c4478ba7e9515d018b4
Rohan Mathur <rohanmathur34@gmail.com>
2013-06-29 13:07:05 -0400

    evita: update build fingerprint
    Change-Id: I476e8b38d0b742358ae5fcd39d8d59ca25f6c2f2
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- products/evita.mk
+++ products/evita.mk
@@ -20,7 +20,7 @@ PRODUCT_MODEL := One X
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_evita BUILD_FINGERPRINT=cingular_us/evita/evita:4.0.4/IMM76D/79936.7:user/release-keys PRIVATE_BUILD_DESC="2.20.502.7 CL79936 release-keys" BUILD_NUMBER=79936
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_evita BUILD_FINGERPRINT=cingular_us/evita/evita:4.1.1/JRO03C/131981.6:user/release-keys PRIVATE_BUILD_DESC="3.18.502.6 CL131981 release-keys" BUILD_NUMBER=79936
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip 
