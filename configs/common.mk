f036f575ec24511b86203fdf8be2a078c8805841
altan <korukaltan@gmail.com>
2013-07-21 00:27:46 +0300

    Build ntfs-3g, and exfat tools
    Change-Id: I348d197226c05415e638d382b2c49bd80cc80f24

--- configs/common.mk
+++ configs/common.mk
@@ -85,17 +85,22 @@ vendor/rr/prebuilt/ResurrectionRemix-changelog.txt:system/etc/ResurrectionRemix-
 vendor/rr/prebuilt/common/etc/persist.conf:system/etc/persist.conf	    
 
 PRODUCT_PACKAGES += \
-    openvpn \
     e2fsck \
-    mke2fs \
-    tune2fs \
+    fsck.exfat \
     libssh \
+    mke2fs \
+    mkfs.exfat \
+    mount.exfat \
+    ntfsfix \
+    ntfs-3g \
+    openvpn \
+    scp \
+    sftp \
     ssh \
     sshd \
     sshd-config \
     ssh-keygen \
-    sftp \
-    scp
+    tune2fs
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
