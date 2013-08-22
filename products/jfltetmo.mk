cfcb213bbcdcd62dd6800320b14df46a1d7dfa9a
altan <korukaltan@gmail.com>
2013-08-22 18:53:43 +0300

    fix mako build
    Change-Id: Ie1aae677eb6bded36493737ce9cbdbe5956a7f9e

--- products/jfltetmo.mk
+++ products/jfltetmo.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/rr/configs/common.mk)
 $(call inherit-product, vendor/rr/configs/common_versions.mk)
 
 # Inherit common jf overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/aokp/overlay/jf-common
+DEVICE_PACKAGE_OVERLAYS += vendor/rr/overlay/jf-common
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
