db5d0ab48a29fa7d237014a6766d4893104f558e
westcripp <korukaltan@gmail.com>
2013-12-01 23:49:37 +0200

    push new vendor

--- configs/common_tablet.mk
+++
@@ -1,5 +0,0 @@
-# Inherit common stuff
-$(call inherit-product, vendor/rr/configs/common.mk)
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/rr/overlay/common_tablet
-
--- configs/themes_common.mk
+++
@@ -1,9 +0,0 @@
-# T-Mobile theme engine
-PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes \
-       Achromatic
-
-PRODUCT_COPY_FILES += \
-       vendor/rr/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
