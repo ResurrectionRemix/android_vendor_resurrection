fa815ffaa041a9b0d4fc2422c70bd6ae5dd50174
westcripp <korukaltan@gmail.com>
2013-12-01 23:57:20 +0200

    test

--- config/common.mk
+++ config/common.mk
@@ -15,6 +15,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.com.android.dataroaming=false \
 	persist.sys.root_access=1
 
+
 # enable ADB authentication if not on eng build
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
