c11527970df7d16a4688b89ff918090edb1f7590
Steven Luo <steven@steven676.net>
2014-01-04 16:59:27 -0800

    Don't ship CellBroadcastReceiver on wifi-only devices
    Attempt to fix dependency issue
    
    Change-Id: I0122fc4665827bfcefd61e4287d9ff685b675aee
    
    Conflicts:
    
    	config/telephony.mk

--- config/telephony.mk
+++ config/telephony.mk
@@ -7,7 +7,15 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # Telephony packages
+<<<<<<< HEAD
   PRODUCT_PACKAGES += \
      Mms \
      Stk \
      WhisperPush
+=======
+PRODUCT_PACKAGES += \
+    Mms \
+    Stk \
+    CellBroadcastReceiver \
+    WhisperPush
+>>>>>>> 4913344... Don't ship CellBroadcastReceiver on wifi-only devices
