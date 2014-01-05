13a67bfe4a251cf1f9ba8fc26a4871ff5e3f1ede
Steven Luo <steven@steven676.net>
2014-01-04 17:09:19 -0800

    Remove some optional packages from "mini" builds
    Change-Id: I31f502a10d64343c8800d51df28b6de49e44bccc

--- config/common.mk
+++ config/common.mk
@@ -126,8 +126,6 @@ PRODUCT_PACKAGES += \
 # Optional CM packages
 PRODUCT_PACKAGES += \
     VoicePlus \
-    VoiceDialer \
-    SoundRecorder \
     Basic \
     libemoji
 
@@ -161,7 +159,6 @@ PRODUCT_PACKAGES += \
     mke2fs \
     tune2fs \
     bash \
-    vim \
     nano \
     htop \
     powertop \
