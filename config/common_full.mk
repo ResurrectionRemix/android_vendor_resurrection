71eb58619202f1322aee375384a71d9bf1f63aaf
Shubhang <shubhang92@gmail.com>
2014-01-30 20:59:29 +0530

    remove video files

--- config/common_full.mk
+++ config/common_full.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
+# $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
