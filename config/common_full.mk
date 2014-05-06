564e5da61edccb60f433d5a832582b08daa18b7e
Steve Kondik <shade@chemlab.org>
2014-05-06 03:20:37 -0700

    cm: Build Stagefright FFMPEG plugin
    Change-Id: Ic727bcea4d5cbc35c720800660be7b80d638122d

--- config/common_full.mk
+++ config/common_full.mk
@@ -33,6 +33,13 @@ PRODUCT_PACKAGES += \
     libvideoeditor_videofilters \
     libvideoeditorplayer
 
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libstagefright_soft_ffmpegadec.so \
+    libstagefright_soft_ffmpegvdec.so \
+    libFFmpegExtractor.so \
+    libnamparser.so
+
 # Extra tools in CM
 PRODUCT_PACKAGES += \
     vim
