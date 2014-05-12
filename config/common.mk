443f8e1d233c0a39a62e74db201bcaea6561763c
Shubhang Rathore <xenon92@users.noreply.github.com>
2014-05-12 09:34:29 +0530

    THIS IS RR! (1/2)

--- config/common.mk
+++ config/common.mk
@@ -68,7 +68,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
+    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-RR.txt
 
 # RR Stuff
 PRODUCT_COPY_FILES += \
