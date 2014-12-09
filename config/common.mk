010a111ad99d21aed7135fa239f73ec129d8616e
westcripp <korukaltan@gmail.com>
2014-12-09 23:55:25 +0200

    add RR-CHANGELOG
    Change-Id: Id9aa7d2148a9d3c6ecefa780b0a0bc75aa41972c

--- config/common.mk
+++ config/common.mk
@@ -80,7 +80,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
+    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-RR.txt
 
 # Backup Tool
 ifneq ($(WITH_GMS),true)
