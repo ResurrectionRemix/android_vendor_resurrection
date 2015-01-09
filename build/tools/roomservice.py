6dc0f3ee24181c0f9bd8f707339691b66158a108
Anthony King <anthonydking@slimroms.net>
2015-01-09 18:04:22 +0000

    Overhaul roomservice for sanity and cleanliness
    I wish anyone porting this over to other versions of roomservice luck
    
    Change-Id: I372d254469f5b3306e0a5e370d26d22ed6e49eaa
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Copyright (C) 2012-2013, The SlimRoms Project
+# Copyright (C) 2012-2013, The CyanogenMod Project
+# Copyright (C) 2012-2015, SlimRoms Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,82 +20,68 @@ import base64
 import json
 import netrc
 import os
-import re
 import sys
-try:
-  # For python3
-  import urllib.error
-  import urllib.parse
-  import urllib.request
-except ImportError:
-  # For python2
-  import imp
-  import urllib2
-  import urlparse
-  urllib = imp.new_module('urllib')
-  urllib.error = urllib2
-  urllib.parse = urlparse
-  urllib.request = urllib2
 
 from xml.etree import ElementTree
 
-product = sys.argv[1];
-
-if len(sys.argv) > 2:
-    depsonly = sys.argv[2]
-else:
-    depsonly = None
-
 try:
-    device = product[product.index("_") + 1:]
-except:
-    device = product
+    # For python3
+    import urllib.error
+    import urllib.parse
+    import urllib.request
+except ImportError:
+    # For python2
+    import imp
+    import urllib2
+    import urlparse
+    urllib = imp.new_module('urllib')
+    urllib.error = urllib2
+    urllib.parse = urlparse
+    urllib.request = urllib2
 
-if not depsonly:
-    print("Device %s not found. Attempting to retrieve device repository from SlimRoms Github (http://github.com/SlimRoms)." % device)
+default_manifest = ".repo/manifest.xml"
 
-repositories = []
+custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
+custom_default_revision = "lp5.0"
+custom_dependencies = "slim.dependencies"
+org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
+org_display = "SlimRoms"  # needed for displaying
 
-try:
-    authtuple = netrc.netrc().authenticators("api.github.com")
+github_auth = None
 
-    if authtuple:
-        githubauth = base64.encodestring('%s:%s' % (authtuple[0], authtuple[2])).replace('\n', '')
-    else:
-        githubauth = None
-except:
-    githubauth = None
 
-def add_auth(githubreq):
-    if githubauth:
-        githubreq.add_header("Authorization","Basic %s" % githubauth)
+local_manifests = '.repo/local_manifests'
+if not os.path.exists(local_manifests):
+    os.makedirs(local_manifests)
 
-if not depsonly:
-    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:SlimRoms+in:name+fork:true" % device)
-    add_auth(githubreq)
-    try:
-        result = json.loads(urllib.request.urlopen(githubreq).read().decode())
-    except urllib.error.URLError:
-        print("Failed to search GitHub")
-        sys.exit()
-    except ValueError:
-        print("Failed to parse return data from GitHub")
-        sys.exit()
-    for res in result.get('items', []):
-        repositories.append(res)
 
-local_manifests = r'.repo/local_manifests'
-if not os.path.exists(local_manifests): os.makedirs(local_manifests)
+def add_auth(g_req):
+    global github_auth
+    if github_auth is None:
+        try:
+            auth = netrc.netrc().authenticators("api.github.com")
+        except (netrc.NetrcParseError, IOError):
+            auth = None
+        if auth:
+            github_auth = base64.b64encode(
+                ('%s:%s' % (auth[0], auth[2])).encode()
+            )
+        else:
+            github_auth = ""
+    if github_auth:
+        g_req.add_header("Authorization", "Basic %s" % github_auth)
 
-def exists_in_tree(lm, repository):
+
+def exists_in_tree(lm, repo):
     for child in lm.getchildren():
-        if child.attrib['name'].endswith(repository):
+        if child.attrib['name'].endswith(repo):
             return True
     return False
 
-# in-place prettyprint formatter
+
 def indent(elem, level=0):
-    i = "\n" + level*"  "
+    # in-place prettyprint formatter
+    i = "\n" + "  " * level
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = i + "  "
@@ -109,20 +96,27 @@ def indent(elem, level=0):
             elem.tail = i
 
 
+def load_manifest(manifest):
+    try:
+        man = ElementTree.parse(manifest).getroot()
+    except (IOError, ElementTree.ParseError):
+        man = ElementTree.Element("manifest")
+    return man
+
+
 def get_default(manifest=None):
-    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    m = manifest or load_manifest(default_manifest)
     d = m.findall('default')[0]
     return d
 
 
 def get_default_revision(manifest=None):
-    m = manifest or ElementTree.parse(".repo/manifest.xml")
-    r = get_default(manifest=m).get('revision')
+    r = get_default(manifest=manifest).get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
 
 
 def get_remote(manifest=None, remote_name=None):
-    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    m = manifest or load_manifest(default_manifest)
     if not remote_name:
         remote_name = get_default(manifest=m).get('remote')
     remotes = m.findall('remote')
@@ -132,10 +126,10 @@ def get_remote(manifest=None, remote_name=None):
 
 
 def get_revision(manifest=None, p="android_build"):
-    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    m = manifest or load_manifest(default_manifest)
     project = None
-    for proj in m.findall("project"):
-        if re.search(r"%s$" % p, proj.get("name")):
+    for proj in m.findall('project'):
+        if proj.get('name').endswith(p):
             project = proj
             break
     if project is None:
@@ -150,70 +144,37 @@ def get_revision(manifest=None, p="android_build"):
     return revision.replace('refs/heads/', '').replace('refs/tags/', '')
 
 
-def get_from_manifest(devicename):
-    try:
-        lm = ElementTree.parse(".repo/local_manifests/slim_manifest.xml")
-        lm = lm.getroot()
-    except:
-        lm = ElementTree.Element("manifest")
-
-    for localpath in lm.findall("project"):
-        if re.search("device_.*_%s$" % device, localpath.get("name")):
-            return localpath.get("path")
-
-    # Devices originally from AOSP are in the main manifest...
-    try:
-        mm = ElementTree.parse(".repo/manifest.xml")
-        mm = mm.getroot()
-    except:
-        mm = ElementTree.Element("manifest")
-
-    for localpath in mm.findall("project"):
-        if re.search("device_.*_%s$" % device, localpath.get("name")):
-            return localpath.get("path")
-
+def get_from_manifest(device_name):
+    for man in (custom_local_manifest, default_manifest):
+        man = load_manifest(man)
+        for local_path in man.findall("project"):
+            lp = local_path.get("name")
+            if lp.startswith("device_") and lp.endswith("_" + device_name):
+                return local_path.get("path")
     return None
 
-def is_in_manifest(projectname):
-    try:
-        lm = ElementTree.parse(".repo/local_manifests/slim_manifest.xml")
-        lm = lm.getroot()
-    except:
-        lm = ElementTree.Element("manifest")
 
-    for localpath in lm.findall("project"):
-        if localpath.get("name") == projectname:
-            return 1
+def is_in_manifest(project_name):
+    for man in (custom_local_manifest, default_manifest):
+        man = load_manifest(man)
+        for local_path in man.findall("project"):
+            if local_path.get("name") == project_name:
+                return True
+    return False
 
-    ## Search in main manifest, too
-    try:
-        lm = ElementTree.parse(".repo/manifest.xml")
-        lm = lm.getroot()
-    except:
-        lm = ElementTree.Element("manifest")
 
-    for localpath in lm.findall("project"):
-        if localpath.get("name") == projectname:
-            return 1
+def add_to_manifest(repos, fallback_branch=None):
+    lm = load_manifest(custom_local_manifest)
 
-    return None
-
-def add_to_manifest(repositories, fallback_branch = None):
-    try:
-        lm = ElementTree.parse(".repo/local_manifests/slim_manifest.xml")
-        lm = lm.getroot()
-    except:
-        lm = ElementTree.Element("manifest")
-
-    for repository in repositories:
-        repo_name = repository['repository']
-        repo_target = repository['target_path']
+    for repo in repos:
+        repo_name = repo['repository']
+        repo_target = repo['target_path']
         if exists_in_tree(lm, repo_name):
-            print('%s already exists' % (repo_name))
+            print('%s already exists' % repo_name)
             continue
 
         if "/" not in repo_name:
-            repo_name = "%s/%s" % ("SlimRoms", repo_name)
+            repo_name = os.path.join(org_manifest, repo_name)
 
         print('Adding dependency: %s -> %s' % (repo_name, repo_target))
 
@@ -224,132 +185,190 @@ def add_to_manifest(repositories, fallback_branch = None):
                     "name": "%s" % repo_name}
         )
 
-        if 'branch' in repository:
-            project.set('revision', repository['branch'])
+        if 'branch' in repo:
+            project.set('revision', repo['branch'])
         elif fallback_branch:
-            print("Using fallback branch %s for %s" %
+            print("Using branch %s for %s" %
                   (fallback_branch, repo_name))
             project.set('revision', fallback_branch)
         else:
             print("Using default branch for %s" % repo_name)
         lm.append(project)
 
-    indent(lm, 0)
-    raw_xml = ElementTree.tostring(lm).decode()
-    raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml
+    indent(lm)
+    raw_xml = "\n".join(('<?xml version="1.0" encoding="UTF-8"?>',
+                         ElementTree.tostring(lm).decode()))
 
-    f = open('.repo/local_manifests/slim_manifest.xml', 'w')
+    f = open(custom_local_manifest, 'w')
     f.write(raw_xml)
     f.close()
 
 _fetch_dep_cache = []
 
 
-def fetch_dependencies(repo_path, fallback_branch = None):
+def fetch_dependencies(repo_path, fallback_branch=None):
     global _fetch_dep_cache
     if repo_path in _fetch_dep_cache:
         return
     _fetch_dep_cache.append(repo_path)
 
     print('Looking for dependencies')
-    dependencies_path = repo_path + '/slim.dependencies'
-    syncable_repos = []
 
-    if os.path.exists(dependencies_path):
-        dependencies_file = open(dependencies_path, 'r')
-        dependencies = json.loads(dependencies_file.read())
-        fetch_list = []
-
-        for dependency in dependencies:
-            repo_name = dependency['repository']
-            if "/" not in repo_name:
-                repo_name = "%s/%s" % ("SlimRoms", repo_name)
-
-            if not is_in_manifest(repo_name):
-                fetch_list.append(dependency)
-                syncable_repos.append(dependency['target_path'])
-
-        dependencies_file.close()
-
-        if len(fetch_list) > 0:
-            print('Adding dependencies to manifest')
-            add_to_manifest(fetch_list, fallback_branch)
+    dep_p = '/'.join((repo_path, custom_dependencies))
+    if os.path.exists(dep_p):
+        with open(dep_p) as dep_f:
+            dependencies = json.load(dep_f)
     else:
+        dependencies = {}
         print('Dependencies file not found, bailing out.')
 
-    if len(syncable_repos) > 0:
+    fetch_list = []
+    syncable_repos = []
+
+    for dependency in dependencies:
+        repo_name = dependency['repository']
+        if '/' not in repo_name:
+            repo_name = os.path.join(org_manifest, repo_name)
+        if not is_in_manifest(repo_name):
+            fetch_list.append(dependency)
+            syncable_repos.append(dependency['target_path'])
+
+    if fetch_list:
+        print('Adding dependencies to manifest')
+        add_to_manifest(fetch_list, fallback_branch)
+
+    if syncable_repos:
         print('Syncing dependencies')
         os.system('repo sync %s' % ' '.join(syncable_repos))
 
     for deprepo in syncable_repos:
         fetch_dependencies(deprepo)
 
+
 def has_branch(branches, revision):
-    return revision in [branch['name'] for branch in branches]
+    return revision in (branch['name'] for branch in branches)
 
-if depsonly:
-    repo_path = get_from_manifest(device)
-    if repo_path:
-        fetch_dependencies(repo_path)
-    else:
-        print("Trying dependencies-only mode on a non-existing device tree?")
 
+def detect_revision(repo):
+    """
+    returns None if using the default revision, else return
+    the branch name if using a different revision
+    """
+    print("Checking branch info")
+    githubreq = urllib.request.Request(
+        repo['branches_url'].replace('{/branch}', ''))
+    add_auth(githubreq)
+    result = json.loads(urllib.request.urlopen(githubreq).read().decode())
+
+    calc_revision = get_revision()
+    default_revision = get_default_revision()
+    print("Calculated revision: %s" % calc_revision)
+    print("Default revision: %s" % default_revision)
+
+
+    if has_branch(result, calc_revision):
+        return calc_revision
+    if has_branch(result, default_revision):
+        return None
+
+    # Try tags, too, since that's what releases use
+    githubreq = urllib.request.Request(
+        repo['tags_url'].replace('{/tag}', ''))
+    add_auth(githubreq)
+    result.extend(json.loads(
+        urllib.request.urlopen(githubreq).read().decode()))
+
+    if has_branch(result, calc_revision):
+        return calc_revision
+    if has_branch(result, default_revision):
+        return None
+
+    fallbacks = os.getenv('ROOMSERVICE_BRANCHES', '').split()
+    for fallback in fallbacks:
+        if has_branch(result, fallback):
+            print("Using fallback branch: %s" % fallback)
+            return fallback
+
+    if has_branch(result, custom_default_revision):
+        print("Falling back to custom revision: %s"
+              % custom_default_revision)
+        return custom_default_revision
+
+    print("Default revision %s not found in %s. Bailing." %
+          (default_revision, repo['name']))
+    print("Branches found:")
+    for branch in result:
+        print(branch['name'])
+    print("Use the ROOMSERVICE_BRANCHES environment variable to "
+          "specify a list of fallback branches.")
     sys.exit()
 
-else:
+
+def main():
+    try:
+        depsonly = bool(sys.argv[2])
+    except IndexError:
+        depsonly = False
+
+    product = sys.argv[1]
+    device = product[product.find("_") + 1:] or product
+
+    if depsonly:
+        repo_path = get_from_manifest(device)
+        if repo_path:
+            fetch_dependencies(repo_path)
+        else:
+            print("Trying dependencies-only mode on a"
+                  "non-existing device tree?")
+        sys.exit()
+
+    print("Device {0} not found. Attempting to retrieve device repository from "
+          "{1} Github (http://github.com/{1}).".format(device, org_display))
+
+    githubreq = urllib.request.Request(
+        "https://api.github.com/search/repositories?"
+        "q={0}+user:{1}+in:name+fork:true".format(device, org_display))
+    add_auth(githubreq)
+
+    repositories = []
+
+    try:
+        result = json.loads(urllib.request.urlopen(githubreq).read().decode())
+    except urllib.error.URLError:
+        print("Failed to search GitHub")
+        sys.exit()
+    except ValueError:
+        print("Failed to parse return data from GitHub")
+        sys.exit()
+    for res in result.get('items', []):
+        repositories.append(res)
+
     for repository in repositories:
         repo_name = repository['name']
-        if repo_name.startswith("device_") and repo_name.endswith("_" + device):
-            print("Found repository: %s" % repository['name'])
-            
-            manufacturer = repo_name.replace("device_", "").replace("_" + device, "")
-            
-            calc_revision = get_revision()
-            default_revision = get_default_revision()
-            print("Default revision: %s" % default_revision)
-            print("Checking branch info")
-            githubreq = urllib.request.Request(repository['branches_url'].replace('{/branch}', ''))
-            add_auth(githubreq)
-            result = json.loads(urllib.request.urlopen(githubreq).read().decode())
+        if not (repo_name.startswith("device_") and
+                repo_name.endswith("_" + device)):
+            continue
+        print("Found repository: %s" % repository['name'])
 
-            ## Try tags, too, since that's what releases use
-            if not (has_branch(result, calc_revision) or has_branch(result, default_revision)):
-                githubreq = urllib.request.Request(repository['tags_url'].replace('{/tag}', ''))
-                add_auth(githubreq)
-                result.extend(json.loads(urllib.request.urlopen(githubreq).read().decode()))
-            
-            repo_path = "device/%s/%s" % (manufacturer, device)
-            adding = {'repository':repo_name,'target_path':repo_path}
-            
-            fallback_branch = None
-            if calc_revision != default_revision and has_branch(result, calc_revision):
-                fallback_branch = calc_revision
-            if not fallback_branch and not has_branch(result, default_revision):
-                if os.getenv('ROOMSERVICE_BRANCHES'):
-                    fallbacks = list(filter(bool, os.getenv('ROOMSERVICE_BRANCHES').split(' ')))
-                    for fallback in fallbacks:
-                        if has_branch(result, fallback):
-                            print("Using fallback branch: %s" % fallback)
-                            fallback_branch = fallback
-                            break
+        fallback_branch = detect_revision(repository)
+        manufacturer = repo_name[7:-(len(device)+1)]
+        repo_path = "device/%s/%s" % (manufacturer, device)
+        adding = [{'repository': repo_name, 'target_path': repo_path}]
 
-                if not fallback_branch:
-                    print("Default revision %s not found in %s. Bailing." % (default_revision, repo_name))
-                    print("Branches found:")
-                    for branch in [branch['name'] for branch in result]:
-                        print(branch)
-                    print("Use the ROOMSERVICE_BRANCHES environment variable to specify a list of fallback branches.")
-                    sys.exit()
+        add_to_manifest(adding, fallback_branch)
 
-            add_to_manifest([adding], fallback_branch)
+        print("Syncing repository to retrieve project.")
+        os.system('repo sync %s' % repo_path)
+        print("Repository synced!")
 
-            print("Syncing repository to retrieve project.")
-            os.system('repo sync %s' % repo_path)
-            print("Repository synced!")
+        fetch_dependencies(repo_path, fallback_branch)
+        print("Done")
+        sys.exit()
 
-            fetch_dependencies(repo_path, fallback_branch)
-            print("Done")
-            sys.exit()
+    print("Repository for %s not found in the %s Github repository list."
+          % (device, org_display))
+    print("If this is in error, you may need to manually add it to your "
+          "%s" % custom_local_manifest)
 
-print("Repository for %s not found in the SlimRoms Github repository list. If this is in error, you may need to manually add it to your local_manifests/slim_manifest.xml." % device)
-
+if __name__ == "__main__":
+    main()
