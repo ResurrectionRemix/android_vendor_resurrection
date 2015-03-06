55b08a8fbcaacd4c9818afafc5c46420ac167ca3
westcripp <korukaltan@gmail.com>
2015-03-06 15:57:16 +0200

    add SU
    Change-Id: I39fb95be050a8c0b1938b2fef111cc098cc4f63f

--- config/common.mk
+++ config/common.mk
@@ -144,7 +144,12 @@ PRODUCT_PACKAGES += \
     LockClock \
     CMAccount \
     CMHome
-
+    
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+    
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
