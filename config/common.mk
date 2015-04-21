b764fc3be2977b9ceb496cac5bee25b269450b90
westcripp <korukaltan@gmail.com>
2015-04-22 02:03:18 +0300

    Revert "Turn art optimization to everything"
    This reverts commit aa0ca4b71c73d2fe9120e3e0f494efc7d127c6a7.

--- config/common.mk
+++ config/common.mk
@@ -60,9 +60,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
-    dalvik.vm.image-dex2oat-filter=everything \
-    dalvik.vm.dex2oat-filter=everything
+    ro.setupwizard.gservices_delay=-1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
