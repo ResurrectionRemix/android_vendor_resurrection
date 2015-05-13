ccff47e6b7fcc5ec7e2d454fc50b9fdddabf3277
Adnan Begovic <adnan@cyngn.com>
2015-05-13 03:57:05 -0700

    cm: Ignore filters for platform library.
    Change-Id: Iaf06872676e9e2745cb2f5ea7dcf1caf71443cb6

--- config/common.mk
+++ config/common.mk
@@ -164,13 +164,11 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
-ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
 # CM Platform Library
 PRODUCT_PACKAGES += \
     org.cyanogenmod.platform-res \
     org.cyanogenmod.platform \
     org.cyanogenmod.platform.xml
-endif
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
