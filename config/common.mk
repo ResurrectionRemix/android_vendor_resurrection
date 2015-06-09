1e56278d2bb5c7ab0d5d1b54c93689572f2eb3db
westcripp <korukaltan@gmail.com>
2015-06-09 04:49:05 +0300

    Update v5.4.7
    Change-Id: Id598beb98222f8066f257420006819ec33ef3aac

--- config/common.mk
+++ config/common.mk
@@ -276,7 +276,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := Resurrection_Remix_LP_v5.4.6
+    CM_BUILDTYPE := Resurrection_Remix_LP_v5.4.7
     CM_EXTRAVERSION :=
 endif
 
