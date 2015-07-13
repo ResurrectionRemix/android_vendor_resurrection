988844f7614e9c338b9c7e9abbf8d25748be7609
Adnan Begovic <adnan@cyngn.com>
2015-07-13 16:16:55 -0700

    vendor/cm: Bump cmsdk version.
    Change-Id: Id1690423ea1754558119209390c24bfb1270920f

--- config/common.mk
+++ config/common.mk
@@ -332,6 +332,16 @@ endif
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 2
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(CM_DISPLAY_VERSION)
 
