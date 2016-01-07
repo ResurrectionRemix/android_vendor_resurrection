309f1fe47e2c952d203373ea9ab3ded9e741e933
Randall Rushing <randall.rushing@gmail.com>
2016-01-07 04:49:53 -0800

    Introduce SmartBar navigation implementation for DirtyUnicorns
    Change-Id: I57cb9ac266458c6d16a8efa33acd732318fa6cb4
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- prebuilt/common/framework/Android.mk
+++ prebuilt/common/framework/Android.mk
@@ -3,7 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     org.dirtyunicorns.navigation-static:org.dirtyunicorns.navigation-static.jar \
-    trail-drawing:trail-core-lib-1.0.4-SNAPSHOT.jar
+    trail-drawing:trail-core-lib-1.0.4-SNAPSHOT.jar \
+    rebound:rebound-0.3.8.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
