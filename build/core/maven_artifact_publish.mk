namdeptrai2003/android_vendor_dnd

cm: Fixup maven artifact publish inclusions.
TICKET: CYNGNOS-1529
Change-Id: Ie7307d43f0b38b179ae42b7c54a790bb7e4c5ea5

build/core/maven_artifact_publish.mk
@@ -15,21 +15,20 @@
 full_target := $(call doc-timestamp-for,$(LOCAL_MODULE))
 
 ifeq ($(strip $(LOCAL_MAVEN_POM)),)
-  $(error LOCAL_MAVEN_POM not defined.)
+  $(warning LOCAL_MAVEN_POM not defined.)
 endif
 ifeq ($(strip $(LOCAL_MAVEN_REPO)),)
-  $(error LOCAL_MAVEN_REPO not defined.)
-endif
-ifeq ($(strip $(LOCAL_MAVEN_TARGET_MODULE)),)
-  $(error LOCAL_MAVEN_TARGET_MODULE not defined.)
+  $(warning LOCAL_MAVEN_REPO not defined.)
 endif
 ifeq ($(strip $(LOCAL_MAVEN_REPO_ID)),)
-  $(error LOCAL_MAVEN_REPO_ID not defined.)
+  $(warning LOCAL_MAVEN_REPO_ID not defined.)
 endif
 
 $(full_target): pomfile := $(LOCAL_MAVEN_POM)
 $(full_target): repo := $(LOCAL_MAVEN_REPO)
+ifdef LOCAL_MAVEN_TARGET_MODULE
 $(full_target): path_to_file := $(call intermediates-dir-for,JAVA_LIBRARIES,$(LOCAL_MAVEN_TARGET_MODULE),,COMMON)/javalib.jar
+endif
 $(full_target): repoId := $(LOCAL_MAVEN_REPO_ID)
 $(full_target): classifier := $(LOCAL_MAVEN_CLASSIFIER)
 $(full_target): sources := $(LOCAL_MAVEN_SOURCES)
@@ -37,6 +36,7 @@ $(full_target): javadoc := $(LOCAL_MAVEN_JAVADOC)
 $(full_target): artifact_path := $(LOCAL_MAVEN_ARTIFACT_PATH)
 $(full_target): artifact_path ?= $(basename $(path_to_file))
 
+ifdef LOCAL_MAVEN_TARGET_MODULE
 $(full_target): $(LOCAL_MAVEN_TARGET_MODULE) $(path_to_file) $(artifact_path) $(ACP)
 	@echo -e ${CL_GRN}"Renaming generated sdk javalib jar"${CL_RST}
 	$(hide) $(ACP) $(path_to_file) $(artifact_path)
@@ -50,4 +50,5 @@ $(full_target): $(LOCAL_MAVEN_TARGET_MODULE) $(path_to_file) $(artifact_path) $(
 			-Dsources=$(sources) \
 			-Djavadoc=$(javadoc)
 	@echo -e ${CL_GRN}"Publishing:"${CL_RST}" $@"
+endif
 $(LOCAL_MODULE): $(full_target)

ResurrectionRemix/android_vendor_resurrection

build/core/maven_artifact_publish.mk
@@ -15,27 +15,26 @@
 full_target := $(call doc-timestamp-for,$(LOCAL_MODULE))
 
 ifeq ($(strip $(LOCAL_MAVEN_POM)),)
-  $(error LOCAL_MAVEN_POM not defined.)
+  $(warning LOCAL_MAVEN_POM not defined.)
 endif
 ifeq ($(strip $(LOCAL_MAVEN_REPO)),)
-  $(error LOCAL_MAVEN_REPO not defined.)
-endif
-ifeq ($(strip $(LOCAL_MAVEN_FILE_PATH)),)
-  $(error LOCAL_MAVEN_FILE_PATH not defined.)
+  $(warning LOCAL_MAVEN_REPO not defined.)
 endif
 ifeq ($(strip $(LOCAL_MAVEN_REPO_ID)),)
-  $(error LOCAL_MAVEN_REPO_ID not defined.)
+  $(warning LOCAL_MAVEN_REPO_ID not defined.)
 endif
-
 
 $(full_target): pomfile := $(LOCAL_MAVEN_POM)
 $(full_target): repo := $(LOCAL_MAVEN_REPO)
+ifdef LOCAL_MAVEN_FILE_PATH
 $(full_target): path_to_file := $(LOCAL_MAVEN_FILE_PATH)
+endif
 $(full_target): repoId := $(LOCAL_MAVEN_REPO_ID)
 $(full_target): classifier := $(LOCAL_MAVEN_CLASSIFIER)
 $(full_target): sources := $(LOCAL_MAVEN_SOURCES)
 $(full_target): javadoc := $(LOCAL_MAVEN_JAVADOC)
 
+ifdef LOCAL_MAVEN_FILE_PATH
 $(full_target):
 	$(hide) mvn -e -X gpg:sign-and-deploy-file \
 		    -DpomFile=$(pomfile) \
@@ -46,4 +45,5 @@ $(full_target):
 			-Dsources=$(sources) \
 			-Djavadoc=$(javadoc)
 	@echo -e ${CL_GRN}"Publishing:"${CL_RST}" $@"
-$(LOCAL_MODULE) : $(full_target)
+endif
+$(LOCAL_MODULE): $(full_target)