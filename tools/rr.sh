35be79d9df5c58ad5671ff9789c3c76677759b52
westcripp <korukaltan@gmail.com>
2016-02-23 11:43:32 +0200

    Improve script

--- tools/rr.sh
+++ tools/rr.sh
@@ -20,44 +20,47 @@ normal='tput sgr0'
 tput setaf 1
 tput bold
 echo -e "";
+echo -e " Build is Succesfully Done !!";
 echo -e "";
-echo -e "          _____                    _____          ";
-echo -e "         /\    \                  /\    \         ";
-echo -e "        /::\    \                /::\    \        ";
-echo -e "       /::::\    \              /::::\    \       ";
-echo -e "      /::::::\    \            /::::::\    \      ";
+echo -e "";
+echo -e "           _____                    _____          ";
+echo -e "          /\    \                  /\    \         ";
+echo -e "         /::\    \                /::\    \        ";
+echo -e "        /::::\    \              /::::\    \       ";
+echo -e "       /::::::\    \            /::::::\    \      ";
 tput setaf 6
-echo -e "     /:::/\:::\    \          /:::/\:::\    \     ";
-echo -e "    /:::/__\:::\    \        /:::/__\:::\    \    ";
-echo -e "   /::::\   \:::\    \      /::::\   \:::\    \   ";
+echo -e "      /:::/\:::\    \          /:::/\:::\    \     ";
+echo -e "     /:::/__\:::\    \        /:::/__\:::\    \    ";
+echo -e "    /::::\   \:::\    \      /::::\   \:::\    \   ";
 tput setaf 7
-echo -e "  /::::::\   \:::\    \    /::::::\   \:::\    \  ";
-echo -e " /:::/\:::\   \:::\____\  /:::/\:::\   \:::\____\ ";
-echo -e "/:::/  \:::\   \:::|    |/:::/  \:::\   \:::|    |";
+echo -e "   /::::::\   \:::\    \    /::::::\   \:::\    \  ";
+echo -e "  /:::/\:::\   \:::\____\  /:::/\:::\   \:::\____\ ";
+echo -e " /:::/  \:::\   \:::|    |/:::/  \:::\   \:::|    |";
 tput setaf 3
-echo -e "\::/   |::::\  /:::|____|\::/   |::::\  /:::|____|";
-echo -e " \/____|:::::\/:::/    /  \/____|:::::\/:::/    / ";
-echo -e "       |:::::::::/    /         |:::::::::/    /  ";
+echo -e " \::/   |::::\  /:::|____|\::/   |::::\  /:::|____|";
+echo -e "  \/____|:::::\/:::/    /  \/____|:::::\/:::/    / ";
+echo -e "        |:::::::::/    /         |:::::::::/    /  ";
 tput setaf 5
-echo -e "       |::|\::::/    /          |::|\::::/    /   ";
-echo -e "       |::| \::/____/           |::| \::/____/    ";
-echo -e "       |::|  ~|                 |::|  ~|          ";
+echo -e "        |::|\::::/    /          |::|\::::/    /   ";
+echo -e "        |::| \::/____/           |::| \::/____/    ";
+echo -e "        |::|  ~|                 |::|  ~|          ";
 tput setaf 4
-echo -e "       |::|   |                 |::|   |          ";
-echo -e "       \::|   |                 \::|   |          ";
-echo -e "        \:|   |                  \:|   |          ";
-echo -e "         \|___|                   \|___|          ";
+echo -e "        |::|   |                 |::|   |          ";
+echo -e "        \::|   |                 \::|   |          ";
+echo -e "         \:|   |                  \:|   |          ";
+echo -e "          \|___|                   \|___|          ";
 echo -e "                                                  ";
 echo -e "                                                  ";
 echo -e "                                                  ";
 tput setaf 1
 tput bold
-echo -e "     R E S U R R E C T I O N   R E M I X  O S";
+echo -e " ██▒▒░░ R E S U R R E C T I O N  R E M I X  O S ░░▒▒██";
 echo -e "                                                  ";
 echo -e "";
-tput setaf 7
+tput sgr0
+tput setaf 4
 tput bold
-echo -e "              M A R S H M A L L O W";
+echo -e " Powered by ANDROID™ // MARSHMALLOW";
 echo -e "";
 tput sgr0
 echo -e "";
