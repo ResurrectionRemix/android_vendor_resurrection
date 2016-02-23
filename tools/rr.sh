183a0622933d5335d57b9d489423b24d3e4e8fb6
westcripp <korukaltan@gmail.com>
2016-02-23 08:00:59 +0200

    Multi colored RR logo script

--- tools/rr.sh
+++ tools/rr.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Specify colors for shell
+red='tput setaf 1'              # red
+green='tput setaf 2'            # green
+yellow='tput setaf 3'           # yellow
+blue='tput setaf 4'             # blue
+violet='tput setaf 5'           # violet
+cyan='tput setaf 6'             # cyan
+white='tput setaf 7'            # white
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) # Bold red
+bldgrn=${txtbld}$(tput setaf 2) # Bold green
+bldblu=${txtbld}$(tput setaf 4) # Bold blue
+bldcya=${txtbld}$(tput setaf 6) # Bold cyan
+normal='tput sgr0'
+
 ./vendor/cm/tools/colors
 
-tput setaf 6
-tput bold
+tput setaf 1
 echo -e "";
 echo -e "";
 echo -e "          _____                    _____          ";
@@ -11,25 +25,33 @@ echo -e "         /\    \                  /\    \         ";
 echo -e "        /::\    \                /::\    \        ";
 echo -e "       /::::\    \              /::::\    \       ";
 echo -e "      /::::::\    \            /::::::\    \      ";
+tput setaf 6
 echo -e "     /:::/\:::\    \          /:::/\:::\    \     ";
 echo -e "    /:::/__\:::\    \        /:::/__\:::\    \    ";
 echo -e "   /::::\   \:::\    \      /::::\   \:::\    \   ";
+tput setaf 7
 echo -e "  /::::::\   \:::\    \    /::::::\   \:::\    \  ";
 echo -e " /:::/\:::\   \:::\____\  /:::/\:::\   \:::\____\ ";
 echo -e "/:::/  \:::\   \:::|    |/:::/  \:::\   \:::|    |";
+tput setaf 3
 echo -e "\::/   |::::\  /:::|____|\::/   |::::\  /:::|____|";
 echo -e " \/____|:::::\/:::/    /  \/____|:::::\/:::/    / ";
 echo -e "       |:::::::::/    /         |:::::::::/    /  ";
 echo -e "       |::|\::::/    /          |::|\::::/    /   ";
 echo -e "       |::| \::/____/           |::| \::/____/    ";
 echo -e "       |::|  ~|                 |::|  ~|          ";
+tput setaf 4
 echo -e "       |::|   |                 |::|   |          ";
 echo -e "       \::|   |                 \::|   |          ";
 echo -e "        \:|   |                  \:|   |          ";
 echo -e "         \|___|                   \|___|          ";
 echo -e "                                                  ";
-echo -e "     R E S U R R E C T I O N   R E M I X";
+tput setaf 1
+tput bold
+echo -e "     R E S U R R E C T I O N   R E M I X  O S";
 echo -e "";
-echo -e "            M A R S H M A L L O W";
+tput setaf 7
+tput bold
+echo -e "              M A R S H M A L L O W";
 echo -e "";
 echo -e "";
