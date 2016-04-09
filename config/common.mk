7de05df9d89dd86a924fc42e2daee524debde590
westcripp <korukaltan@gmail.com>
2016-04-09 14:30:19 +0300

    Update Changelog v5.6.7

--- config/common.mk
+++ config/common.mk
@@ -240,7 +240,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
-PRODUCT_VERSION = 5.6.6
+PRODUCT_VERSION = 5.6.7
     CM_VERSION := ResurrectionRemix-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
