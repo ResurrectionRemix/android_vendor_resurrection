f96e6d8c5346dbb09d33e78e898abcd77d034ac6
Blackjubel <blackjubels@gmail.com>
2016-05-20 19:33:36 +0200

    Update 5.6.9 (#1) (#2)
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -241,7 +241,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
-PRODUCT_VERSION = 5.6.8
+PRODUCT_VERSION = 5.6.9
     CM_VERSION := ResurrectionRemix-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
