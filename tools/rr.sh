363f197674b7d8a1909b735feed911e10b9651bf
westcripp <korukaltan@gmail.com>
2016-06-12 01:08:51 +0300

    Update build final script

--- tools/rr.sh
+++ tools/rr.sh
@@ -17,50 +17,60 @@ normal='tput sgr0'
 
 ./vendor/cm/tools/colors
 
-tput setaf 1
-tput bold
-echo -e "";
-echo -e " Build is Succesfully Done !!";
+
 echo -e "";
 echo -e "";
-echo -e "           _____                    _____          ";
-echo -e "          /\    \                  /\    \         ";
-echo -e "         /::\    \                /::\    \        ";
-echo -e "        /::::\    \              /::::\    \       ";
-echo -e "       /::::::\    \            /::::::\    \      ";
-tput setaf 6
-echo -e "      /:::/\:::\    \          /:::/\:::\    \     ";
-echo -e "     /:::/__\:::\    \        /:::/__\:::\    \    ";
-echo -e "    /::::\   \:::\    \      /::::\   \:::\    \   ";
-tput setaf 7
-echo -e "   /::::::\   \:::\    \    /::::::\   \:::\    \  ";
-echo -e "  /:::/\:::\   \:::\____\  /:::/\:::\   \:::\____\ ";
-echo -e " /:::/  \:::\   \:::|    |/:::/  \:::\   \:::|    |";
-tput setaf 3
-echo -e " \::/   |::::\  /:::|____|\::/   |::::\  /:::|____|";
-echo -e "  \/____|:::::\/:::/    /  \/____|:::::\/:::/    / ";
-echo -e "        |:::::::::/    /         |:::::::::/    /  ";
-tput setaf 5
-echo -e "        |::|\::::/    /          |::|\::::/    /   ";
-echo -e "        |::| \::/____/           |::| \::/____/    ";
-echo -e "        |::|  ~|                 |::|  ~|          ";
 tput setaf 4
-echo -e "        |::|   |                 |::|   |          ";
-echo -e "        \::|   |                 \::|   |          ";
-echo -e "         \:|   |                  \:|   |          ";
-echo -e "          \|___|                   \|___|          ";
-echo -e "                                                  ";
-echo -e "                                                  ";
-echo -e "                                                  ";
+echo -e "              GGGGGGGGGGGGG       OOOOOOOOO       TTTTTTTTTTTTTTTTTTTTTTT";
+echo -e "           GGG::::::::::::G     OO:::::::::OO     T:::::::::::::::::::::T";
+echo -e "         GG:::::::::::::::G   OO:::::::::::::OO   T:::::::::::::::::::::T";
+tput setaf 2
+echo -e "        G:::::GGGGGGGG::::G  O:::::::OOO:::::::O  T:::::TT:::::::TT:::::T";
+echo -e "       G:::::G       GGGGGG  O::::::O   O::::::O  TTTTTT  T:::::T  TTTTTT";
+tput setaf 3
+echo -e "      G:::::G                O:::::O     O:::::O          T:::::T        ";
+echo -e "      G:::::G                O:::::O     O:::::O          T:::::T        ";
+echo -e "      G:::::G    GGGGGGGGGG  O:::::O     O:::::O          T:::::T        ";
 tput setaf 1
-tput bold
-echo -e " ██▒▒░░ R E S U R R E C T I O N  R E M I X  O S ░░▒▒██";
-echo -e "                                                  ";
+echo -e "      G:::::G    G::::::::G  O:::::O     O:::::O          T:::::T        ";
+echo -e "      G:::::G    GGGGG::::G  O:::::O     O:::::O          T:::::T        ";
+echo -e "      G:::::G        G::::G  O:::::O     O:::::O          T:::::T        ";
+tput setaf 5
+echo -e "       G:::::G       G::::G  O::::::O   O::::::O          T:::::T        ";
+echo -e "        G:::::GGGGGGGG::::G  O:::::::OOO:::::::O        TT:::::::TT      ";
+echo -e "         GG:::::::::::::::G   OO:::::::::::::OO         T:::::::::T      ";
+tput setaf 7
+echo -e "           GGG::::::GGG:::G     OO:::::::::OO           T:::::::::T      ";
+echo -e "              GGGGGG   GGGG       OOOOOOOOO             TTTTTTTTTTT  ";
+echo -e "";
+echo -e "";
+tput setaf 7
+echo -e "   88888  88888 888888 8   8 88888  88888  88888 888888 88888 88888 888888 ";
+tput setaf 2
+echo -e "   8   8  8     8      8   8 8   8  8   8  8     8    8   8   8     8    8 ";
+tput setaf 3
+echo -e "   8eee8e 8eeee 8eeeee 8e  8 8eee8e 8eee8e 8eeee 8e       8e  8eeee 8e   8 ";
+tput setaf 1
+echo -e "   88   8 88        88 88  8 88   8 88   8 88    88       88  88    88   8 ";
+tput setaf 4
+echo -e "   88   8 88    e   88 88  8 88   8 88   8 88    88   e   88  88    88   8 ";
+tput setaf 5
+echo -e "   88   8 88eee 8eee88 88ee8 88   8 88   8 88eee 88eee8   88  88eee 88eee8 ";
+echo -e " ";
+
+
+echo -e "";
 echo -e "";
 tput sgr0
-tput setaf 4
-tput bold
-echo -e " Powered by ANDROID™ // MARSHMALLOW";
+tput setaf 3
 echo -e "";
+echo -e " √ Build is Succesfully Done! ";
+echo -e "";
+
+tput setaf 4
+echo -e " 〉Powered by ANDROID ™  // MARSHMALLOW";
+echo -e "";
+tput setaf 1
+echo -e " 〉Enjoy the Resurrection Remix OS // #GETRESURRECTED ";
 tput sgr0
 echo -e "";
