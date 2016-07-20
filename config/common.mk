427b9f92dfc7aa65539e8c21c2c38e02558fc8de
d34d <clark@cyngn.com>
2016-07-20 13:19:32 -0700

    config: Add ThemeManagerService to PRODUCT_PACKAGES
    Change-Id: I8f858b3932ffd557cd752918fbb059caf3770a42

--- config/common.mk
+++ config/common.mk
@@ -140,7 +140,8 @@ PRODUCT_PACKAGES += \
     CMAudioService \
     Development \
     BluetoothExt \
-    Profiles
+    Profiles \
+    ThemeManagerService
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
