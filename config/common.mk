6b0ed25cc8f8d8ea5834396ed25f42ff554036f6
AndropaX <kpax.mar@gmail.com>
2016-07-28 16:01:49 +0200

    separate unofficial and official versions

--- config/common.mk
+++ config/common.mk
@@ -244,7 +244,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 5.7.1
-    CM_VERSION := ResurrectionRemix-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+ifeq ($(RR_RELEASE), OFFICIAL)    
+    CM_VERSION := RR-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-OFFICIAL
+else
+    CM_VERSION := RR-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-UNOFFICIAL
+endif
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.rr.version=$(CM_VERSION) \
