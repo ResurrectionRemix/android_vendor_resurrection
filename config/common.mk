aadb980c056f82e085cbaaab12a35848ffb67cee
Arne Coucheron <arco68@gmail.com>
2016-09-09 00:04:09 +0200

    cm: Minor config reorganization
     * Move extra tools to common.mk. These are needed for CMFM, and should
       be included for all.
     * Move PhotoTable, LiveWallpapersPicker, SoundRecorder and Screencast
       to common.mk. These apps are extremely small, and only takes up
       less than 1 MB combined.
     * Remove obsolete live wallpapers.
     * Move sound property overrides to common.mk. There's no need
       to have them spread across several files.
     * Include CM audio files from common.mk
    
    Change-Id: If308506cd9cd0a75014de988c242d700db5d16f3

--- config/common.mk
+++ config/common.mk
@@ -55,6 +55,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
 ifneq ($(TARGET_BUILD_VARIANT),user)
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
@@ -115,6 +120,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
 
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
 # Theme engine
 include vendor/cm/config/themes_common.mk
 
@@ -135,7 +143,9 @@ PRODUCT_PACKAGES += \
 # Optional CM packages
 PRODUCT_PACKAGES += \
     libemoji \
-    Terminal
+    Terminal \
+    LiveWallpapersPicker \
+    PhotoTable
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
@@ -153,7 +163,9 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     ExactCalculator \
     LiveLockScreenService \
-    WeatherProvider
+    WeatherProvider \
+    SoundRecorder \
+    Screencast
 
 # Exchange support
 PRODUCT_PACKAGES += \
@@ -174,7 +186,18 @@ PRODUCT_PACKAGES += \
     oprofiled \
     sqlite3 \
     strace \
-    pigz
+    pigz \
+    7z \
+    lib7z \
+    bash \
+    bzip2 \
+    curl \
+    powertop \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
 
 # Custom off-mode charger
 ifneq ($(WITH_CM_CHARGER),false)
