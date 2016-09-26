2921b3157e0d4de670234be384532101b39391c2
Varun Date <date.varun123@gmail.com>
2016-09-26 13:14:09 +0530

    Vendor:Enable Media Codecs Again
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -232,15 +232,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rsync
 
-# Stagefright FFMPEG plugin
-#PRODUCT_PACKAGES += \
-#    libffmpeg_extractor \
-#    libffmpeg_omx \
-#    media_codecs_ffmpeg.xml
-#
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    media.sf.omx-plugin=libffmpeg_omx.so \
-#    media.sf.extractor-plugin=libffmpeg_extractor.so
+#Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+     libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.sf.omx-plugin=libffmpeg_omx.so \
+    media.sf.extractor-plugin=libffmpeg_extractor.so
 
 # These packages are excluded from user builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
