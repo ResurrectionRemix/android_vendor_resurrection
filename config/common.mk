43666839eb36d8a05e2b0337bac9244655eb1649
Alexander Martinz <eviscerationls@gmail.com>
2016-12-01 18:57:48 +0100

    cm: Fix Trusted Face
    Trusted Face depends on libprotobuf-cpp-full.
    Some devices already ship it (eg RIL needs it).
    
    Include it here to build it for every device.
    
    Link to OpenGapps issue:
    https://github.com/opengapps/opengapps/issues/390
    
    Change-Id: Iffee0aa6218dfef45f5cd728a999fa1114c0dd57
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -170,8 +170,9 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     PhotoTable
 
-# Include librsjni explicitly to workaround GMS issue
+# Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
     librsjni
 
 # Custom CM packages
