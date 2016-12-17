8ade97995617f8224bc45897a9a69cd2a01ac2e6
Akhil Narang <akhilnarang.1999@gmail.com>
2016-12-17 17:16:00 +0100

    vendor: add RR_BUILDTYPE
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -293,7 +293,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 5.8.0
-    CM_VERSION := ResurrectionRemix-N-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+RR_BUILDTYPE ?= UNOFFICIAL
+CM_VERSION := ResurrectionRemix-N-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-$(RR_BUILDTYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.rr.version=$(CM_VERSION) \
