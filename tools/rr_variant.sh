8d59e5e92f039ef197a294971c01e8e60cac1d53
westcripp <altan_koruk@hotmail.com>
2016-12-24 00:59:18 +0300

    Script: Add 3 more build-type
    - Release      ︱ Only for the official releases by the RR-team
    
    - Milestone    ︱ Only for the milestone releases by the RR-team
    
    - Final        ︱ Only for the final releases by the RR-team
    
    - Update descriptions

--- tools/rr_variant.sh
+++ tools/rr_variant.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # Build type chooser for the Resurrection Remix ROM
 . $ANDROID_BUILD_TOP/vendor/cm/tools/colors
-echo -e ${ylw}"\n\n ▼ Which build type do you want to choose?\n"${txtrst}
+echo -e ${ylw}"\n\n ▼ Which build-type do you want to choose?\n"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 1- Official "${txtrst}${red}"    ︱ Option for the only official device maintainers"${txtrst}
+echo -e ${blu}" 〉 1- Official "${txtrst}${red}"    ︱ Only for the official builds by the maintainers"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 2- Unofficial "${txtrst}${red}"  ︱ Option for the unofficial device builders"${txtrst}
+echo -e ${blu}" 〉 2- Unofficial "${txtrst}${red}"  ︱ For the unofficial builds"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 3- Nightly "${txtrst}${red}"     ︱ Option for the only official daily builders"${txtrst}
+echo -e ${blu}" 〉 3- Nightly "${txtrst}${red}"     ︱ Only for the official daily builds by the maintainers"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 4- Weekly "${txtrst}${red}"      ︱ Option for the only official weekly builders"${txtrst}
+echo -e ${blu}" 〉 4- Weekly "${txtrst}${red}"      ︱ Only for the official weekly builds by the maintainers"${txtrst}
 echo -e "";
-echo -e ${blu}" 🕑  30/sec Time-out "${txtrst}${red}"︱ Default"${txtrst}
+echo -e ${blu}" 〉 5- Release "${txtrst}${red}"     ︱ Only for the official releases by the RR-team"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 6- Milestone "${txtrst}${red}"   ︱ Only for the milestone releases by the RR-team"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 7- Final "${txtrst}${red}"       ︱ Only for the final releases by the RR-team"${xtrst}
+echo -e "";
+echo -e ${blu}" 🕑  30/sec Time-out "${txtrst}${red}"︱ Default option"${txtrst}
 echo -e "";
 echo -e ${cya}" ▼ Pick a number"${txtrst}
 echo -e "";
@@ -53,6 +59,36 @@ then
 echo -e "";
 echo -e ${blu}" ▪ Building weekly variant "${txtrst}
 export RR_BUILDTYPE=weekly
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "5" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building release variant "${txtrst}
+export RR_BUILDTYPE=release
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "6" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building milestone variant "${txtrst}
+export RR_BUILDTYPE=milestone
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "7" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building final variant "${txtrst}
+export RR_BUILDTYPE=final
 echo -e "";
 echo -e "";
 $normal
