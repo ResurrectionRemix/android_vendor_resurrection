6565364053953864f19e22392f95efb349f31dcd
Varun Date <date.varun123@gmail.com>
2016-12-25 09:42:50 +0530

    Set a Prop for RR Build Type [2/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -300,6 +300,7 @@ CM_VERSION := RR-N-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)$(RR_
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.rr.version=$(CM_VERSION) \
  ro.modversion=$(CM_VERSION) \
+ rr.build.type=$(RR_BUILDTYPE) \
  rr.ota.version= $(shell date -u +%Y%m%d) \
  ro.romstats.url=http://resurrectionremix.sourceforge.net/ \
  ro.romstats.name=ResurrectionRemix \
