628d42f9fdf9d2c94b8ae5457f51d21e8593ede2
Hriday Sharma <hridaysharma42@gmail.com>
2017-02-28 21:22:07 +0000

    Revert "Disable SU by default"
    This reverts commit 1947310619aaf9f37fca8957d3fb24ffef3fcb2c.
    
    We do not need to disable SU,
    We are RR. <3

--- config/common.mk
+++ config/common.mk
@@ -261,13 +261,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PACKAGES += \
     procmem \
-    procrank
-
-# Conditionally build in su
-ifeq ($(WITH_SU),true)
-PRODUCT_PACKAGES += \
+    procrank \
     su
-endif
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
