579649f9f9ca76f184e0894b8baa14d37269b1a1
Alex Naidis <alex.naidis@linux.com>
2017-05-16 04:11:40 +0000

    Recommend using the non-debug dexpreopter
    * can still be overwritten

--- config/common.mk
+++ config/common.mk
@@ -71,6 +71,9 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG := false
+
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     CHANGELOG.mkdn:system/etc/RR/Changelog.txt
