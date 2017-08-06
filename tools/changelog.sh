e74670c3dd32183ae9fbc933a262bdcff432f357
westcripp <korukaltan@gmail.com>
2017-08-06 20:29:01 +0300

    Update changelog to 5.8.4

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -54,7 +54,7 @@ echo -e "";
 read -r -t 30 days_to_log || days_to_log=7
 fi
 echo >> $Changelog;
-echo " ▼ $source_name Ver 5.8.3 Changelog"    >> $Changelog;
+echo " ▼ $source_name Ver 5.8.4 Changelog"    >> $Changelog;
 echo '' >> $Changelog;
 echo >> $Changelog;
 
