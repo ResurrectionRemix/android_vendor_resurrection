fb4c6c6eb4428c26f120c4cf4c01a7a044f6c54d
Aaron Kling <webgeek1234@gmail.com>
2017-12-07 13:55:52 -0600

    ATV: Ship AppDrawer
    Also remove TvSettings from here since its included in atv_base
    
    Change-Id: I2d97e90e234dc9b7868dad3b063c0420211dacdc
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,6 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_full.mk)
 
-PRODUCT_PACKAGES += TvSettings
+PRODUCT_PACKAGES += AppDrawer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/tv
