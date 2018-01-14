11df5ffb39cb856b55a2d1b2bffe7a6618f39d1e
Michael W <baddaemon87@gmail.com>
2018-01-14 18:46:02 +0100

    envsetup: Clean up for install[boot|recovery]
    * Remove the files from device after installation
    
    Change-Id: I52dc60c22abde972a066b879b3a74459261966e5

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -369,6 +369,7 @@ function installboot()
             adb shell chmod 644 /system/lib/modules/*
         fi
         adb shell dd if=/cache/boot.img of=$PARTITION
+        adb shell rm -rf /cache/boot.img
         echo "Installation complete."
     else
         echo "The connected device does not appear to be $RR_BUILD, run away!"
@@ -409,6 +410,7 @@ function installrecovery()
     then
         adb push $OUT/recovery.img /cache/
         adb shell dd if=/cache/recovery.img of=$PARTITION
+        adb shell rm -rf /cache/recovery.img
         echo "Installation complete."
     else
         echo "The connected device does not appear to be $RR_BUILD, run away!"
