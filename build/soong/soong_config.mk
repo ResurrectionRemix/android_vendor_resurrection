a422937900d0338ef650f39b3f62a88af0e35515
Varun Date <date.varun123@gmail.com>
2018-01-17 17:18:38 +1100

    fix build [3/3]
    Change-Id: I7105a57ba4607c8bb0fb471a17a52af9d746d114
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,9 +1,9 @@
 # Insert new variables inside the Lineage structure
-rr_soong:
+lineage_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"RR": {'; \
+	echo '"Lineage": {'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
