8cb26dd87982695604cdca0f94f88291b647dbac
Varun Date <date.varun123@gmail.com>
2018-01-17 23:16:49 +1100

    Fix derped boot
    Change-Id: Ia346d33796e0fecf2b02a0fabea4a3c6380fe288
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -104,42 +104,13 @@ include vendor/rr/config/twrp.mk
 endif
 
 # Bootanimation
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/rr/prebuilt/common/bootanimation | sort -rn))
-
-# find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
-    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
-
-ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
-PRODUCT_BOOTANIMATION := vendor/rr/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-else
-PRODUCT_BOOTANIMATION := vendor/rr/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-endif
-endif
+PRODUCT_PACKAGES += \
+    bootanimation.zip
 
 # Required rr packages
 PRODUCT_PACKAGES += \
     BluetoothExt \
-    rrParts \
+    LineageParts \
     Development \
     Profiles
 
@@ -158,8 +129,7 @@ PRODUCT_PACKAGES += \
 # Custom rr packages
 PRODUCT_PACKAGES += \
     AudioFX \
-    rrSettingsProvider \
-    rrSetupWizard \
+    LineageSettingsProvider \
     Eleven \
     ExactCalculator \
     Jelly \
