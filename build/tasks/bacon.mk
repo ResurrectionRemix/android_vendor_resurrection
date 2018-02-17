f2aef2804ede1ae3c0fec89c18ac54989b7e86ce
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-17 10:24:40 -0200

    Copy changelog to out folder

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -21,6 +21,7 @@ RR_TARGET_PACKAGE := $(PRODUCT_OUT)/$(RR_VERSION).zip
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(RR_TARGET_PACKAGE)
+	$(hide) mv $(PRODUCT_OUT)/system/etc/RR/Changelog.txt $(PRODUCT_OUT)/$(RR_VERSION)-Changelog.txt
 	$(hide) $(MD5SUM) $(RR_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(RR_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(RR_TARGET_PACKAGE)" >&2
 	$(hide) ./vendor/rr/tools/rr.sh
