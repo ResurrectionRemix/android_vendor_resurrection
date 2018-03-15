4abddb01af56a746e9035217cfb701a115a3a30a
westcripp <korukaltan@gmail.com>
2018-03-15 11:22:19 +0300

    Add properties for build flash info script
    - Copy properties to build.prop
    
    Change-Id: If3998e3ea074eb8c3f3c2cb7253d2e4521af11a6

--- config/common.mk
+++ config/common.mk
@@ -320,6 +320,16 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     rr.ota.version= $(shell date +%Y%m%d) \
     ro.rr.tag=$(shell grep "refs/tags" .repo/manifest.xml  | cut -d'"' -f2 | cut -d'/' -f3)
 
+# Properties for build flash info script
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rr.version=$(RR_VERSION) \
+    ro.rr.releasetype=$(RR_BUILDTYPE) \
+    ro.rr.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(RR_VERSION) \
+    rr.build.type=$(RR_BUILDTYPE) \
+    rr.ota.version= $(shell date +%Y%m%d) \
+    ro.rr.tag=$(shell grep "refs/tags" .repo/manifest.xml  | cut -d'"' -f2 | cut -d'/' -f3)
+    
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/rr/build/target/product/security/rr
 
