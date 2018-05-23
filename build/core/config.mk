4945c55387df06166cdc95e963162f7dd0e311b2
Ethan Chen <intervigil@gmail.com>
2018-05-22 23:40:12 -0700

    build: Remove HTTP_PREBUILT
    * It appears the only user of this was Gello, which is no longer using
      it.
    * Remove this deprecated directive
    
    Change-Id: I6ab869c2f2c3b891b58b9a750e7ce882f169f296
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/core/config.mk
+++ build/core/config.mk
@@ -20,6 +20,4 @@ FRAMEWORK_LINEAGE_PLATFORM_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_current.
 FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_removed.txt
 FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/rr/build/core/apicheck_msg_current.txt
 
-BUILD_HTTP_PREBUILT := $(TOP)/vendor/rr/build/core/http_prebuilt.mk
-
 BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/rr/build/core/system_rro.mk
