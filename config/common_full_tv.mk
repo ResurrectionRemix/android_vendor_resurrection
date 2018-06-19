46f261ba49c7c5c78d0c38621ac33856f8f59403
Aaron Kling <webgeek1234@gmail.com>
2018-06-19 12:10:10 -0500

    ATV: Build LineageCustomizer
    Change-Id: I1a9e18516441dd513378bc5332b181b4e059e19b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,8 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_full.mk)
 
-PRODUCT_PACKAGES += AppDrawer
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/rr/overlay/tv
