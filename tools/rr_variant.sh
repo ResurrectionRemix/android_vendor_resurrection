5ca74b1cfd8a923702bf227740ba510ae0979b50
mracar07 <mr.acar07@gmail.com>
2018-07-11 18:36:52 +0000

    Remove the Official selection from script
    Tested with lunch device
    
    Change-Id: I6aa05ddd743af7bfe891087b1a935a5c52d2fba9

--- tools/rr_variant.sh
+++ tools/rr_variant.sh
@@ -24,21 +24,19 @@
 
 echo -e ${ylw}"\n\n ▼ Which build-type do you want to choose?\n"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 1- Official "${txtrst}${red}"    ︱ Only for the official builds by the maintainers"${txtrst}
+echo -e ${blu}" 〉 1- Unofficial "${txtrst}${red}"  ︱ For the unofficial builds"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 2- Unofficial "${txtrst}${red}"  ︱ For the unofficial builds"${txtrst}
+echo -e ${blu}" 〉 2- Nightly "${txtrst}${red}"     ︱ Only for the official daily builds by the maintainers"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 3- Nightly "${txtrst}${red}"     ︱ Only for the official daily builds by the maintainers"${txtrst}
+echo -e ${blu}" 〉 3- Weekly "${txtrst}${red}"      ︱ Only for the official weekly builds by the maintainers"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 4- Weekly "${txtrst}${red}"      ︱ Only for the official weekly builds by the maintainers"${txtrst}
+echo -e ${blu}" 〉 4- Release "${txtrst}${red}"     ︱ Only for the official releases by the RR-Team"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 5- Release "${txtrst}${red}"     ︱ Only for the official releases by the RR-Team"${txtrst}
+echo -e ${blu}" 〉 5- Milestone "${txtrst}${red}"   ︱ Only for the milestone releases by the RR-Team"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 6- Milestone "${txtrst}${red}"   ︱ Only for the milestone releases by the RR-Team"${txtrst}
+echo -e ${blu}" 〉 6- Experimental "${txtrst}${red}"︱ Only for the experimental releases by the RR-Team"${xtrst}
 echo -e "";
-echo -e ${blu}" 〉 7- Experimental "${txtrst}${red}"︱ Only for the experimental releases by the RR-Team"${xtrst}
-echo -e "";
-echo -e ${blu}" 〉 8- Final "${txtrst}${red}"       ︱ Only for the final releases by the RR-Team"${xtrst}
+echo -e ${blu}" 〉 7- Final "${txtrst}${red}"       ︱ Only for the final releases by the RR-Team"${xtrst}
 echo -e "";
 echo -e ${blu}" 〉 30/sec Time-out"${txtrst}${red}" ︱ Default option"${txtrst}
 echo -e "";
@@ -50,16 +48,6 @@ sleep 1
 if [ "$askvariant" == "1" ]
 then
 echo -e "";
-echo -e ${blu}" ▪ Building official variant "${txtrst}
-export RR_BUILDTYPE=Official
-echo -e "";
-echo -e "";
-$normal
-sleep 1
-fi
-if [ "$askvariant" == "2" ]
-then
-echo -e "";
 echo -e ${blu}" ▪ Building unofficial variant "${txtrst}
 export RR_BUILDTYPE=Unofficial
 echo -e "";
@@ -67,7 +55,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "3" ]
+if [ "$askvariant" == "2" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building nightly variant "${txtrst}
@@ -77,7 +65,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "4" ]
+if [ "$askvariant" == "3" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building weekly variant "${txtrst}
@@ -87,7 +75,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "5" ]
+if [ "$askvariant" == "4" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building release variant "${txtrst}
@@ -97,7 +85,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "6" ]
+if [ "$askvariant" == "5" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building milestone variant "${txtrst}
@@ -107,7 +95,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "7" ]
+if [ "$askvariant" == "6" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building experimental variant "${txtrst}
@@ -117,7 +105,7 @@ echo -e "";
 $normal
 sleep 1
 fi
-if [ "$askvariant" == "8" ]
+if [ "$askvariant" == "7" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building final variant "${txtrst}
