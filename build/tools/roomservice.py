f646eebb8357ecf28683c9a360d7918b2b139e55
mracar07 <umutcan@umutcanacar.me>
2019-01-02 12:54:41 +0100

    Roomservice: Look rr-d instead of rr-devices
    Change-Id: Ic3fa42ae0e0b14c28361dbfa2bf3f6a2e5f8c538
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -34,7 +34,7 @@ default_manifest = ".repo/manifest.xml"
 custom_local_manifest = ".repo/local_manifests/roomservice.xml"
 custom_default_revision = "pie"
 custom_dependencies = "rr.dependencies"
-org_manifest = "rr-devices"  # leave empty if org is provided in manifest
+org_manifest = "rr-d"  # leave empty if org is provided in manifest
 org_display = "ResurrectionRemix-Devices"  # needed for displaying
 
 github_auth = None
