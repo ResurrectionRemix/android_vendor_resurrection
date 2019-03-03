e3c043f46198c9a090ff3987a369bd6c74f7a3df
Felipe Leon <fglfgl27@gmail.com>
2019-03-03 15:40:29 -0300

    Vendor: update CHANGELOG.mkdn and changelog_github.sh for Pie
    Add functions to generate a clickble link for it line
    also remove all line that contains "Merge", and remove empty changes
    
    Move generated CHANGELOG.mkdn to ANDROID_BUILD_TOP to prevent overwrite of vendor files causing repo sync issues
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- tools/changelog_github.sh
+++ tools/changelog_github.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-# Simple sh to automatic generate a file with source and device specif git commit changes to use in a github wiki pages or file.md
-# like this:
-# https://github.com/bhb27/android_vendor_crdroid/blob/change_temp/Changelog.md
-# file.md can work with more data or have more lines then a page wiki
+#!/bin/bash
+# Simple sh to automatic generate a repo changelog
 # Specify colors utilized in the terminal
     red=$(tput setaf 1)             #  red
     grn=$(tput setaf 2)             #  green
@@ -24,67 +21,114 @@
     ylwrev=${rev}$(tput setaf 3)
     blurev=${rev}$(tput setaf 4)
     normal='tput sgr0'
+
 # input variables set the below the rest must be automatic
 source_tree="${ANDROID_BUILD_TOP}"; #path here must be inside home directory
-changelog_path_name=vendor/rr/CHANGELOG.mkdn #changelog file path/name.extension
+changelog_path_name=CHANGELOG.mkdn #changelog file path/name.extension
 source_name="Resurrection Remix Pie" #Name to display in changelog.md top before version
 # input variables end
 
 if [ -z ${source_tree} ]; then
-echo "Please run envsetup.sh first";
+	echo "Please run '. build/envsetup.sh' first";
 else
+	export Changelog=$source_tree/$changelog_path_name
+	export Temp_Changelog=$source_tree/$changelog_path_name.temp
 
-export Changelog=$source_tree/$changelog_path_name
-export Temp_Changelog=$source_tree/$changelog_path_name.temp
-
-if [ -f $Changelog ];
- then
- 	rm -f $Changelog
-fi
-
-touch $Changelog
-
-
-# ask for days and version
-echo ""
-echo ${cya}" ▼ Generating (for last 180 days) github format changelog.."${txtrst}
-echo ""
-echo >> $Changelog
-echo "# $source_name Version 7.0.0 Changelog"    >> $Changelog;
-echo '' >> $Changelog;
-echo >> $Changelog;
-
-cd $source_tree
-
-for i in $(seq 180);
-do
-export After_Date=`date --date="$i days ago" +%m-%d-%Y`
-k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
-	echo ""
-	echo ${ylw}" 〉 Generating day number $i ▪ $Until_Date.."${txtrst}
-	source=$(repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date');
-
-	if [ -n "${source##+([:space:])}" ]; then
-
-		echo "$Until_Date" >> $Changelog;
-		echo '============' >> $Changelog;
-		repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' | grep -v "Automatic translation import" | sed 's/^$/#EL /' | sed 's/^/* /' | sed 's/* #EL //' >> $Changelog
-		echo >> $Changelog;
+	if [ -f $Changelog ]; then
+	 	rm -f $Changelog
 	fi
 
-done
+	touch $Changelog
 
-sed -i 's/* project /#### /g' $Changelog
+	days_to_log=180
 
-Changelog=$HOME/$source_tree/$changelog_path_name
-if [ -f $Changelog ] && [ -f $Temp_Changelog ];
-then
-	echo "$(cat $Temp_Changelog)\n$(cat $Changelog)" > $Changelog
-	rm -f $Temp_Changelog
+	echo ${cya}" ▼ Generating (for last $days_to_log days) github format changelog.."${txtrst}
+
+	echo >> $Changelog;
+	echo "$source_name Version $(grep PRODUCT_VERSION vendor/rr/build/core/main_version.mk | head -1 | cut -d= -f2 | cut -d' ' -f2) Changelog" >> $Changelog;
+	echo '===============' >> $Changelog;
+	echo >> $Changelog;
+
+	cd $source_tree
+
+	contains() {
+		string="$1"
+		substring="$2"
+		if test "${string#*$substring}" != "$string"; then
+		return 0    # $substring is in $string
+		else
+			return 1    # $substring is not in $string
+		fi
+	}
+
+	#https://stackoverflow.com/questions/296536/how-to-urlencode-data-for-curl-command
+	rawurlencode() {
+		local string="${1}"
+		local strlen=${#string}
+		local encoded=""
+		local pos c o
+
+		for (( pos=0 ; pos<strlen ; pos++ )); do
+			c=${string:$pos:1}
+			case "$c" in
+			[-_.~a-zA-Z0-9] ) o="${c}" ;;
+			* )               printf -v o '%%%02x' "'$c"
+			esac
+			encoded+="${o}"
+		done
+		echo "${encoded}"    # You can either set a return variable (FASTER) 
+		REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+	}
+
+	git_log_repo() {
+		repo forall -pc 'git log --oneline --after='$1' --until='$2 | grep -v "Automatic translation import" | grep -v "Merge" | sed 's/^$/#EL /' | sed 's/^/* /' | sed 's/* #EL //' | sed 's/* //' | while read string; do
+			project=0;
+			temp_test="$string"
+			contains "$temp_test" "project" && project=1;
+			if [ "$project" == 1 ]; then
+				addproject=1;
+				projectpath=$string
+			fi
+			if [ -n "${string##+([:space:])}" ]; then
+				if [ "$project" == 0 ]; then
+					if [ "$addproject" == 1 ]; then
+						echo >> $Changelog;
+						echo "* $projectpath" >> $Changelog;
+						addproject=0;
+					fi
+					temp_one=$(echo "$string" | sed 's/^[^ ]* //g')
+					echo "* [$temp_one](https://github.com/search?q=$( rawurlencode "$temp_one" )&type=Commits)" >> $Changelog;
+				fi;
+			fi
+		done
+		echo >> $Changelog;
+		echo "#### $source_name source changes of $Until_Date End." >>  $Changelog;
+		echo >> $Changelog;
+	}
+
+	for i in $(seq $days_to_log);
+	do
+	export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+	k=$(expr $i - 1)
+		export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+
+		echo ${blu}" 〉 Generating day number $i ▪ $Until_Date.."${txtrst}
+
+		source=$(repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' | grep -v "Automatic translation import" | grep -v "Merge" );
+
+		if [ -n "${source##+([:space:])}" ]; then
+			# Line with after --- until was too long for a small ListView
+			echo -e "$Until_Date" >> $Changelog;
+			echo '===============' >> $Changelog;
+			git_log_repo $After_Date $Until_Date
+		fi
+
+	done
+
+	sed -i 's/* project /#### /g' $Changelog
+	echo >> $Changelog;
+
+	echo -e ${ylw}"\n √ Changelog successfully generated (file is $Changelog).\n"${txtrst}
 fi
-echo ""
-echo ${grn}" √ Changelog successfully generated."${txtrst}
-echo ""
-echo ""
-fi
+
+exit;
