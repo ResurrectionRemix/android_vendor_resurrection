db1c6830dbaf05e6ec616194e9b992d26aad0e1d
AlexAnanas <43793572+AlexAnanas@users.noreply.github.com>
2019-03-29 17:53:07 +0300

    update oreo changelog generator

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -46,7 +46,7 @@ if [ -z $days_to_log ];then
 echo ""
 echo ${ylw}" ▼ For how many days changelog do you want to generate?"${txtrst}
 echo ""
-echo -e ${blu}" 〉 30/sec Time-out"${txtrst}${red}" ︱ 7 Days by default"${txtrst}
+echo -e ${blu}" 〉 30/sec Time-out"${txtrst}${red}" ︱ don't generate changelog by default"${txtrst}
 echo -e "";
 echo -e ${cya}" ▼ Type a number"${txtrst}
 echo -e "";
