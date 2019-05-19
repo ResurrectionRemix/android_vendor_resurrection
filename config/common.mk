9a1d97accfe3c8a4b6963983ca4539e967215853
blunden <blunden2@gmail.com>
2019-05-19 12:51:39 +0200

    Actually include legacy privapp whitelist
    Change-Id: I11f4d524a628f3fddb3a6a0c6fe645922123fc89
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- config/common.mk
+++ config/common.mk
@@ -105,7 +105,8 @@ PRODUCT_COPY_FILES += \
 # This is Lineage!
 PRODUCT_COPY_FILES += \
     vendor/rr/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml \
-    vendor/rr/config/permissions/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml
+    vendor/rr/config/permissions/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml \
+    vendor/rr/config/permissions/privapp-permissions-cm-legacy.xml:system/etc/permissions/privapp-permissions-cm-legacy.xml
 
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \
