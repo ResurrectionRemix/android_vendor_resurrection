d4dcf5eff8337e79ff30c32aa2c8f91198b3d866
mracar07 <umutcan@umutcanacar.me>
2019-09-20 09:19:09 +0000

    vendor: rr: fix build prop definations
    Change-Id: I4369897856c74bedfce6286d3ead7ee17fbe1242
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,10 +3,12 @@ ifneq ($(RR_BUILDTYPE),)
 RR_VERSION := RR-v$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-$(RR_BUILDTYPE)
 else
 RR_VERSION := RR-v$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-Unofficial
+RR_BUILDTYPE := Unofficial
 endif
 
 # RR System Version
 
+RR_PROPERTIES := \
     ro.rr.version=$(RR_VERSION) \
     ro.rr.releasetype=$(RR_BUILDTYPE) \
     ro.rr.build.version=$(PRODUCT_VERSION) \
