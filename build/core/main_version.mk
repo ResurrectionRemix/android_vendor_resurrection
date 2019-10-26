8c7361f4f6db53877495b466f63a4173b3cd6638
mracar07 <umutcan@umutcanacar.me>
2019-10-26 21:56:38 +0000

    Fix versioning and permission issue
    Change-Id: Ic147d926826dde8c7492656dd7f8e0f821a62c28
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,8 +1,8 @@
-PRODUCT_VERSION = 7.0.2
+PRODUCT_VERSION = Ten
 ifneq ($(RR_BUILDTYPE),)
-RR_VERSION := RR-P-v$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-$(RR_BUILDTYPE)
+RR_VERSION := RR-$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-$(RR_BUILDTYPE)
 else
-RR_VERSION := RR-P-v$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-Unofficial
+RR_VERSION := RR-$(PRODUCT_VERSION)-$(shell date +%Y%m%d)-$(RR_BUILD)-Unofficial
 RR_BUILDTYPE=Unofficial
 endif
 
