71668978444f34c62de176f212626b3f3cccb313
Aaron Kling <webgeek1234@gmail.com>
2019-11-07 20:10:45 -0600

    Add nvidia product
    Intended to be conditionally included in tegra device trees
    
    This will be used to indicate which tegra specific modifications
    are available. Firstly is the baseline framework modifications
    for nvcpl and profiles
    
    Also change the soong check to use lower case like all the other
    flags
    
    Change-Id: Ibae95de3ee47a4a1cefbcd4035ef48dbc246d266

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -25,7 +25,7 @@ $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_
 $(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
 $(call add_json_bool, Target_camera_needs_client_info, $(filter true,$(TARGET_CAMERA_NEEDS_CLIENT_INFO)))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
-$(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_nvidia_enhancements, $(filter true,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 $(call add_json_bool, Uses_qcom_um_family, $(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
