c988bbe5cf7f81d2a9c24221f9b0dbd5528e8dd1
Varun Date <date.varun123@gmail.com>
2020-04-13 12:39:50 +1000

    Build LongShot
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I994e15753d8efc70d64ed1aa2f049594ed370cee

--- config/common.mk
+++ config/common.mk
@@ -143,7 +143,19 @@ vendor/rr/Features.mkdn:system/etc/RR/Features.txt
 PRODUCT_PACKAGES += \
     OmniJaws \
     OmniStyle \
+    Longshot \
     ThemePicker
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 
 # AOSP packages
 PRODUCT_PACKAGES += \
