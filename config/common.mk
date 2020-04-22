92029132d367d0866a87e4c7a31cc5d8da1cf111
Varun Date <date.varun123@gmail.com>
2020-04-23 03:49:22 +1000

    Vendor_rr:Fix the damn init path
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I6cef79fe0b85a39f95d94efa8aadab488d4a7b6b

--- config/common.mk
+++ config/common.mk
@@ -58,7 +58,7 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Copy all Lineage-specific init rc files
-$(foreach f,$(wildcard vendor/lrr/prebuilt/common/etc/init/*.rc),\
+$(foreach f,$(wildcard vendor/rr/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
