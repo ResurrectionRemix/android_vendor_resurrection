27358a42f77c0efd2283201652015a38fca18b69
TheScarastic <warabhishek@gmail.com>
2020-06-01 17:21:03 +1000

    Rename fod lib flag
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I9f7e6ef0e4e044ce31f8dd31ec50adadcae73a56

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -45,6 +45,7 @@ type Product_variables struct {
 		Cppflags []string
 	}
 	Target_surfaceflinger_fod_lib struct {
+		Cflags []string
 		Whole_static_libs []string
 	}
 	Target_process_sdk_version_override struct {
@@ -112,7 +113,7 @@ type ProductVariables struct {
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
+    Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
 	Target_camera_needs_client_info  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
