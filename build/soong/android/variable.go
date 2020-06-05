8ea3c413fe3997a3891377a174de8c06cc231505
Anushek Prasal <anushekprasal@gmail.com>
2020-06-05 23:07:31 +0530

    vendor: Cleanup QCOM flags
    Revert "vendor: Fix TARGET_USE_AOSP_SURFACEFLINGER logic"
    
    This reverts commit 019a0946c49dec18b0ae855f3f47b96c704fba8f.
    
    Revert "vendor: Allow disabling CAF surfaceflinger"
    
    This reverts commit 0135a228c86dca3a9413b582828dd20c2636ce73.
    
    Revert "vendor: Add back soong and build configuration for qcom platform"
    
    This reverts commit 61524d9e9b96960242357d888c4fba8a2e28d734.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: Icf8a3676ac7f4cb1a51effa25000651cdbe2e66a

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -70,30 +70,6 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
-	Uses_qcom_um_family struct {
-		Cflags []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_3_18_family struct {
-		Include_dirs []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_4_family struct {
-		Include_dirs []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_9_family struct {
-		Include_dirs []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_14_family struct {
-		Include_dirs []string
-		Header_libs []string
-		Shared_libs []string
-	}
 }
 
 type ProductVariables struct {
@@ -119,9 +95,4 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
-	Uses_qcom_um_family  *bool `json:",omitempty"`
-	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
 }
